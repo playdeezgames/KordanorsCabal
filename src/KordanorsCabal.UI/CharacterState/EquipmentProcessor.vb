7c1c3248aa45284ec03e7b7048888127387b62c4
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-07-03 05:51:32 -0500

    i can select an equip slot

--- src/KordanorsCabal.UI/CharacterState/EquipmentProcessor.vb
+++ src/KordanorsCabal.UI/CharacterState/EquipmentProcessor.vb
@@ -1,23 +1,52 @@
 ﻿Friend Class EquipmentProcessor
     Implements IProcessor
 
+    Private rowIndex As Integer = 0
+    Private table As New Dictionary(Of Integer, EquipSlot)
+
     Public Sub UpdateBuffer(buffer As PatternBuffer) Implements IProcessor.UpdateBuffer
         buffer.Fill(Pattern.Space, False, Hue.Black)
         buffer.FillCells((0, 0), (buffer.Columns, 1), Pattern.Space, True, Hue.Blue)
         buffer.WriteTextCentered(0, "Equipment", True, Hue.Blue)
+        buffer.WriteText((0, 1), "Go Back", rowIndex = 0, Hue.Black)
         Dim row As Integer = 1
         Dim player = World.PlayerCharacter
         For Each entry In player.Equipment
-            buffer.WriteText((0, row), $"{entry.Key.Name}: {entry.Value.Name}", False, Hue.Black)
+            buffer.WriteText((0, row + 1), $"{entry.Key.Name}: {entry.Value.Name}", rowIndex = row, Hue.Black)
             row += 1
         Next
         buffer.WriteTextCentered(buffer.Rows - 1, "Arrows, Space, Esc", False, Hue.Black)
     End Sub
 
     Public Sub Initialize() Implements IProcessor.Initialize
+        rowIndex = 0
+        table.Clear()
+        table(0) = EquipSlot.None
+        Dim row As Integer = 1
+        For Each entry In World.PlayerCharacter.Equipment
+            table(row) = entry.Key
+            row += 1
+        Next
     End Sub
 
     Public Function ProcessCommand(command As Command) As UIState Implements IProcessor.ProcessCommand
-        Return UIState.InPlay
+        Select Case command
+            Case Command.Red
+                Return UIState.InPlay
+            Case Command.Up
+                rowIndex = (rowIndex + table.Count - 1) Mod table.Count
+            Case Command.Down
+                rowIndex = (rowIndex + 1) Mod table.Count
+            Case Command.Green, Command.Blue
+                Return InteractEquipSlot(table(rowIndex))
+        End Select
+        Return UIState.Equipment
+    End Function
+
+    Private Function InteractEquipSlot(equipSlot As EquipSlot) As UIState
+        If equipSlot = EquipSlot.None Then
+            Return UIState.InPlay
+        End If
+        Return UIState.Equipment
     End Function
 End Class
