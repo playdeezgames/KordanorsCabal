f0685d286beebd6d6d09a63c5d22068d164e9b0c
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-08-02 04:37:49 -0500

    i hve visted the moon

--- src/KordanorsCabal.Game/Item/Descriptor/Magic/MoonPortalDescriptor.vb
+++ src/KordanorsCabal.Game/Item/Descriptor/Magic/MoonPortalDescriptor.vb
@@ -12,4 +12,19 @@
             Return "Moon Portal"
         End Get
     End Property
+    Public Overrides ReadOnly Property CanUse(character As Character) As Boolean
+        Get
+            Return character.Location.IsDungeon
+        End Get
+    End Property
+
+    Public Overrides Sub Use(character As Character)
+        Dim location = character.Location
+        location.DestroyRoute(Direction.Outward)
+        Dim destination = RNG.FromEnumerable(Game.Location.FromLocationType(LocationType.Moon))
+        destination.DestroyRoute(Direction.Inward)
+        Route.Create(location, Direction.Outward, RouteType.Portal, destination)
+        Route.Create(destination, Direction.Inward, RouteType.Portal, location)
+        character.EnqueueMessage("A portal opens before you!")
+    End Sub
 End Class
