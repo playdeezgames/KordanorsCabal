7e86491d5c1f6944f602fe4a37cb82cb3de2e3e7
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-08-07 04:28:19 -0500

    dont need direction descriptor overrides

--- src/KordanorsCabal.Game/Direction/Descriptor/DirectionDescriptor.vb
+++ src/KordanorsCabal.Game/Direction/Descriptor/DirectionDescriptor.vb
@@ -1,37 +1,37 @@
-﻿Friend MustInherit Class DirectionDescriptor
-    MustOverride ReadOnly Property Opposite As Direction
-    MustOverride ReadOnly Property Name As String
-    Overridable ReadOnly Property IsCardinal As Boolean
-        Get
-            Return False
-        End Get
-    End Property
-    MustOverride ReadOnly Property Abbreviation As String
-
-    Overridable ReadOnly Property NextDirection As Direction?
-        Get
-            Return Nothing
-        End Get
-    End Property
-
-    Overridable ReadOnly Property PreviousDirection() As Direction?
-        Get
-            Return Nothing
-        End Get
-    End Property
+﻿Friend Class DirectionDescriptor
+    Sub New(
+           name As String,
+           abbreviation As String,
+           opposite As Direction,
+           Optional nextDirection As Direction? = Nothing,
+           Optional previousDirection As Direction? = Nothing,
+           Optional isCardinal As Boolean = False)
+        Me.Name = name
+        Me.Abbreviation = abbreviation
+        Me.Opposite = opposite
+        Me.NextDirection = nextDirection
+        Me.PreviousDirection = previousDirection
+        Me.IsCardinal = isCardinal
+    End Sub
+    ReadOnly Property Name As String
+    ReadOnly Property Abbreviation As String
+    ReadOnly Property Opposite As Direction
+    ReadOnly Property IsCardinal As Boolean
+    ReadOnly Property NextDirection As Direction?
+    ReadOnly Property PreviousDirection As Direction?
 End Class
 Friend Module DirectionDescriptorUtility
     Friend DirectionDescriptors As IReadOnlyDictionary(Of Direction, DirectionDescriptor) =
         New Dictionary(Of Direction, DirectionDescriptor) From
         {
-            {Direction.Down, New DownDescriptor},
-            {Direction.East, New EastDescriptor},
-            {Direction.Inward, New InwardDescriptor},
-            {Direction.North, New NorthDescriptor},
-            {Direction.Outward, New OutwardDescriptor},
-            {Direction.South, New SouthDescriptor},
-            {Direction.Up, New UpDescriptor},
-            {Direction.West, New WestDescriptor}
+            {Direction.Down, New DirectionDescriptor("Down", "D", Direction.Up)},
+            {Direction.East, New DirectionDescriptor("East", "E", Direction.West, Direction.South, Direction.North, True)},
+            {Direction.Inward, New DirectionDescriptor("In", "In", Direction.Outward)},
+            {Direction.North, New DirectionDescriptor("North", "N", Direction.South, Direction.East, Direction.West, True)},
+            {Direction.Outward, New DirectionDescriptor("Out", "Out", Direction.Inward)},
+            {Direction.South, New DirectionDescriptor("South", "S", Direction.North, Direction.West, Direction.East, True)},
+            {Direction.Up, New DirectionDescriptor("Up", "U", Direction.Down)},
+            {Direction.West, New DirectionDescriptor("West", "W", Direction.East, Direction.North, Direction.South, True)}
         }
     Friend ReadOnly Property AllDirections As IEnumerable(Of Direction)
         Get
