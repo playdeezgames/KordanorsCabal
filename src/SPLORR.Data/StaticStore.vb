03bfbbd557c3867475f57ae6762379bb222b56d4
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-08-08 17:47:37 -0500

    ReadCountForColumnValue

--- src/SPLORR.Data/StaticStore.vb
+++ src/SPLORR.Data/StaticStore.vb
@@ -1,6 +1,3 @@
 Public Module StaticStore
     Public ReadOnly Store As New Store
-    Public Function ReadCountForColumnValue(Of TInputColumn)(initializer As Action, tableName As String, inputColumnValue As (String, TInputColumn)) As Long
-        Return Store.ReadCountForColumnValue(initializer, tableName, inputColumnValue)
-    End Function
 End Module
