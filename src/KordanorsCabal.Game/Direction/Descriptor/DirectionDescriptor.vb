8f46055dad25533c0754c00c9158ef7a48dc9329
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-08-09 07:51:03 -0500

    next and prev seem to work?

--- src/KordanorsCabal.Game/Direction/Descriptor/DirectionDescriptor.vb
+++ src/KordanorsCabal.Game/Direction/Descriptor/DirectionDescriptor.vb
@@ -1,11 +1,6 @@
 ﻿Friend Class DirectionDescriptor
-    Sub New(
-           directionId As Long,
-           Optional nextDirection As Direction? = Nothing,
-           Optional previousDirection As Direction? = Nothing)
+    Sub New(directionId As Long)
         Id = directionId
-        Me.NextDirection = nextDirection
-        Me.PreviousDirection = previousDirection
     End Sub
     '[Directions]([DirectionId],[DirectionName],[Abbreviation],[IsCardinal],[OppositeDirectionId],[NextDirectionId],[PreviousDirectionId])
     ReadOnly Property Id As Long
@@ -30,20 +25,30 @@
         End Get
     End Property
     ReadOnly Property NextDirection As Direction?
+        Get
+            Dim result = StaticWorldData.World.Direction.ReadNext(Id)
+            Return If(result.HasValue, CType(result.Value, Direction), Nothing)
+        End Get
+    End Property
     ReadOnly Property PreviousDirection As Direction?
+        Get
+            Dim result = StaticWorldData.World.Direction.ReadPrevious(Id)
+            Return If(result.HasValue, CType(result.Value, Direction), Nothing)
+        End Get
+    End Property
 End Class
 Friend Module DirectionDescriptorUtility
     Friend DirectionDescriptors As IReadOnlyDictionary(Of Direction, DirectionDescriptor) =
         New Dictionary(Of Direction, DirectionDescriptor) From
         {
             {Direction.Down, New DirectionDescriptor(Direction.Down)},
-            {Direction.East, New DirectionDescriptor(Direction.East, Direction.South, Direction.North)},
+            {Direction.East, New DirectionDescriptor(Direction.East)},
             {Direction.Inward, New DirectionDescriptor(Direction.Inward)},
-            {Direction.North, New DirectionDescriptor(Direction.North, Direction.East, Direction.West)},
+            {Direction.North, New DirectionDescriptor(Direction.North)},
             {Direction.Outward, New DirectionDescriptor(Direction.Outward)},
-            {Direction.South, New DirectionDescriptor(Direction.South, Direction.West, Direction.East)},
+            {Direction.South, New DirectionDescriptor(Direction.South)},
             {Direction.Up, New DirectionDescriptor(Direction.Up)},
-            {Direction.West, New DirectionDescriptor(Direction.West, Direction.North, Direction.South)}
+            {Direction.West, New DirectionDescriptor(Direction.West)}
         }
     Friend ReadOnly Property AllDirections As IEnumerable(Of Direction)
         Get
