2226edfb8b6494b135d371814c629e9ab3b08d8b
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-08-10 04:00:46 -0500

    esd has an id

--- src/KordanorsCabal.Game/EquipSlot/EquipSlotDescriptor.vb
+++ src/KordanorsCabal.Game/EquipSlot/EquipSlotDescriptor.vb
@@ -1,6 +1,8 @@
 ﻿Public Class EquipSlotDescriptor
+    ReadOnly Property Id As Long
     ReadOnly Property Name As String
-    Sub New(name As String)
+    Sub New(equipSlotId As Long, name As String)
+        Me.Id = equipSlotId
         Me.Name = name
     End Sub
 End Class
@@ -8,13 +10,13 @@ Public Module EquipSlotDescriptorUtility
     Friend EquipSlotDescriptors As IReadOnlyDictionary(Of EquipSlot, EquipSlotDescriptor) =
         New Dictionary(Of EquipSlot, EquipSlotDescriptor) From
         {
-            {EquipSlot.Head, New EquipSlotDescriptor("Head")},
-            {EquipSlot.LeftHand, New EquipSlotDescriptor("LHand")},
-            {EquipSlot.Legs, New EquipSlotDescriptor("Legs")},
-            {EquipSlot.Neck, New EquipSlotDescriptor("Neck")},
-            {EquipSlot.RightHand, New EquipSlotDescriptor("RHand")},
-            {EquipSlot.Shield, New EquipSlotDescriptor("Shield")},
-            {EquipSlot.Torso, New EquipSlotDescriptor("Torso")},
-            {EquipSlot.Weapon, New EquipSlotDescriptor("Weapon")}
+            {EquipSlot.Head, New EquipSlotDescriptor(EquipSlot.Head, "Head")},
+            {EquipSlot.LeftHand, New EquipSlotDescriptor(EquipSlot.LeftHand, "LHand")},
+            {EquipSlot.Legs, New EquipSlotDescriptor(EquipSlot.Legs, "Legs")},
+            {EquipSlot.Neck, New EquipSlotDescriptor(EquipSlot.Neck, "Neck")},
+            {EquipSlot.RightHand, New EquipSlotDescriptor(EquipSlot.RightHand, "RHand")},
+            {EquipSlot.Shield, New EquipSlotDescriptor(EquipSlot.Shield, "Shield")},
+            {EquipSlot.Torso, New EquipSlotDescriptor(EquipSlot.Torso, "Torso")},
+            {EquipSlot.Weapon, New EquipSlotDescriptor(EquipSlot.Weapon, "Weapon")}
         }
 End Module
