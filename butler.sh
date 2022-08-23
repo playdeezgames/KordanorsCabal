0a29820ba90c9da04ed7e1397c599a39770e6b0a
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-08-23 03:24:21 -0500

    butler.sh becomes shippit.sh

--- butler.sh
+++
@@ -1,3 +0,0 @@
-butler push pub-windows thegrumpygamedev/kordanors-cabal:windows
-butler push pub-linux thegrumpygamedev/kordanors-cabal:linux
-
---
+++ shippit.sh
@@ -0,0 +1,4 @@
+dotnet publish ./src/KordanorsCabal/KordanorsCabal.vbproj -o ./pub-linux -c Release --sc -r linux-x64
+dotnet publish ./src/KordanorsCabal/KordanorsCabal.vbproj -o ./pub-windows -c Release --sc -r win-x64
+butler push pub-windows thegrumpygamedev/kordanors-cabal:windows
+butler push pub-linux thegrumpygamedev/kordanors-cabal:linux
