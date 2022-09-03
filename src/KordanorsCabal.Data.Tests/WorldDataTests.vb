0d90c400b14a8ab2f3fee0ca33f7010adb8262ed
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-09-03 05:49:10 -0500

    world data tests

--- src/KordanorsCabal.Data.Tests/WorldDataTests.vb
+++ src/KordanorsCabal.Data.Tests/WorldDataTests.vb
@@ -3,44 +3,86 @@ Namespace KordanorsCabal.Data.Tests
         <Fact>
         Sub ShouldConstructAllOfTheDataSubobjects()
             Dim store As New Mock(Of IStore)
-            Dim worldData As New WorldData(store.Object)
-            worldData.Character.ShouldNotBeNull
-            worldData.CharacterEquipSlot.ShouldNotBeNull
-            worldData.CharacterLocation.ShouldNotBeNull
-            worldData.CharacterQuestCompletion.ShouldNotBeNull
-            worldData.CharacterQuest.ShouldNotBeNull
-            worldData.CharacterSpell.ShouldNotBeNull
-            worldData.CharacterStatistic.ShouldNotBeNull
-            worldData.CharacterStatisticType.ShouldNotBeNull
-            worldData.CharacterType.ShouldNotBeNull
-            worldData.CharacterTypeAttackType.ShouldNotBeNull
-            worldData.CharacterTypeBribe.ShouldNotBeNull
-            worldData.CharacterTypeEnemy.ShouldNotBeNull
-            worldData.CharacterTypeInitialStatistic.ShouldNotBeNull
-            worldData.CharacterTypeLoot.ShouldNotBeNull
-            worldData.CharacterTypePartingShot.ShouldNotBeNull
-            worldData.CharacterTypeSpawnCount.ShouldNotBeNull
-            worldData.CharacterTypeSpawnLocation.ShouldNotBeNull
-            worldData.Direction.ShouldNotBeNull
-            worldData.DungeonLevel.ShouldNotBeNull
-            worldData.EquipSlot.ShouldNotBeNull
-            worldData.Feature.ShouldNotBeNull
-            worldData.FeatureType.ShouldNotBeNull
-            worldData.Inventory.ShouldNotBeNull
-            worldData.InventoryItem.ShouldNotBeNull
-            worldData.Item.ShouldNotBeNull
-            worldData.ItemStatistic.ShouldNotBeNull
-            worldData.ItemType.ShouldNotBeNull
-            worldData.ItemTypeSpawnCount.ShouldNotBeNull
-            worldData.ItemTypeSpawnLocationType.ShouldNotBeNull
-            worldData.ItemTypeStatistic.ShouldNotBeNull
-            worldData.ItemTypeStatisticType.ShouldNotBeNull
-            worldData.Location.ShouldNotBeNull
-            worldData.LocationDungeonLevel.ShouldNotBeNull
-            worldData.LocationStatistic.ShouldNotBeNull
-            worldData.LocationType.ShouldNotBeNull
-            worldData.Player.ShouldNotBeNull
-            worldData.Route.ShouldNotBeNull
+            Dim subject As New WorldData(store.Object)
+            subject.Character.ShouldNotBeNull
+            subject.CharacterEquipSlot.ShouldNotBeNull
+            subject.CharacterLocation.ShouldNotBeNull
+            subject.CharacterQuestCompletion.ShouldNotBeNull
+            subject.CharacterQuest.ShouldNotBeNull
+            subject.CharacterSpell.ShouldNotBeNull
+            subject.CharacterStatistic.ShouldNotBeNull
+            subject.CharacterStatisticType.ShouldNotBeNull
+            subject.CharacterType.ShouldNotBeNull
+            subject.CharacterTypeAttackType.ShouldNotBeNull
+            subject.CharacterTypeBribe.ShouldNotBeNull
+            subject.CharacterTypeEnemy.ShouldNotBeNull
+            subject.CharacterTypeInitialStatistic.ShouldNotBeNull
+            subject.CharacterTypeLoot.ShouldNotBeNull
+            subject.CharacterTypePartingShot.ShouldNotBeNull
+            subject.CharacterTypeSpawnCount.ShouldNotBeNull
+            subject.CharacterTypeSpawnLocation.ShouldNotBeNull
+            subject.Direction.ShouldNotBeNull
+            subject.DungeonLevel.ShouldNotBeNull
+            subject.EquipSlot.ShouldNotBeNull
+            subject.Feature.ShouldNotBeNull
+            subject.FeatureType.ShouldNotBeNull
+            subject.Inventory.ShouldNotBeNull
+            subject.InventoryItem.ShouldNotBeNull
+            subject.Item.ShouldNotBeNull
+            subject.ItemStatistic.ShouldNotBeNull
+            subject.ItemType.ShouldNotBeNull
+            subject.ItemTypeSpawnCount.ShouldNotBeNull
+            subject.ItemTypeSpawnLocationType.ShouldNotBeNull
+            subject.ItemTypeStatistic.ShouldNotBeNull
+            subject.ItemTypeStatisticType.ShouldNotBeNull
+            subject.Location.ShouldNotBeNull
+            subject.LocationDungeonLevel.ShouldNotBeNull
+            subject.LocationStatistic.ShouldNotBeNull
+            subject.LocationType.ShouldNotBeNull
+            subject.Player.ShouldNotBeNull
+            subject.Route.ShouldNotBeNull
+            store.VerifyNoOtherCalls()
+        End Sub
+        <Fact>
+        Sub ShouldSaveToTheStore()
+            Dim store As New Mock(Of IStore)
+            Dim subject As New WorldData(store.Object)
+            Const filename = "filename"
+            subject.Save(filename)
+            store.Verify(Sub(x) x.Save(filename), Times.Once)
+            store.VerifyNoOtherCalls()
+        End Sub
+        <Fact>
+        Sub ShouldLoadFromTheStore()
+            Dim store As New Mock(Of IStore)
+            Dim subject As New WorldData(store.Object)
+            Const filename = "filename"
+            subject.Load(filename)
+            store.Verify(Sub(x) x.Load(filename), Times.Once)
+            store.VerifyNoOtherCalls()
+        End Sub
+        <Fact>
+        Sub ShouldResetTheStore()
+            Dim store As New Mock(Of IStore)
+            Dim subject As New WorldData(store.Object)
+            subject.Reset()
+            store.Verify(Sub(x) x.Reset(), Times.Once)
+            store.VerifyNoOtherCalls()
+        End Sub
+        <Fact>
+        Sub ShouldRenewTheStore()
+            Dim store As New Mock(Of IStore)
+            Dim subject As New WorldData(store.Object)
+            subject.Renew().ShouldBeNull
+            store.Verify(Function(x) x.Renew(), Times.Once)
+            store.VerifyNoOtherCalls()
+        End Sub
+        <Fact>
+        Sub ShouldRestoreTheStore()
+            Dim store As New Mock(Of IStore)
+            Dim subject As New WorldData(store.Object)
+            subject.Restore(Nothing)
+            store.Verify(Sub(x) x.Restore(Nothing), Times.Once)
             store.VerifyNoOtherCalls()
         End Sub
     End Class
