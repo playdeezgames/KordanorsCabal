9af492cc6c880d4b7c9a7e000c5b5049f48bab3a
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-09-05 05:02:54 -0500

    ShouldQueryTheStoreForHowManTimesACharacterHasCompletedAQuest

--- src/KordanorsCabal.Data.Tests/Character/CharacterQuestCompletionDataTests.vb
+++ src/KordanorsCabal.Data.Tests/Character/CharacterQuestCompletionDataTests.vb
@@ -13,4 +13,20 @@
                                  (CharacterIdColumn, characterId)))
             End Sub)
     End Sub
+    <Fact>
+    Sub ShouldQueryTheStoreForHowManTimesACharacterHasCompletedAQuest()
+        WithSubobject(
+            Function(x) x.CharacterQuestCompletion,
+            Sub(store, subject)
+                Dim characterId = 1L
+                Dim questId = 2L
+                subject.Read(characterId, questId).ShouldBeNull
+                store.Verify(Function(x) x.ReadColumnValue(Of Long, Long, Long)(
+                                 It.IsAny(Of Action),
+                                 Tables.CharacterQuestCompletions,
+                                 Columns.CompletionsColumn,
+                                 (CharacterIdColumn, characterId),
+                                 (QuestColumn, questId)))
+            End Sub)
+    End Sub
 End Class
--- src/KordanorsCabal.Data.Tests/Utility/Columns.vb
+++ src/KordanorsCabal.Data.Tests/Utility/Columns.vb
@@ -1,7 +1,9 @@
 ﻿Module Columns
     Friend Const CharacterIdColumn = "CharacterId"
     Friend Const CharacterTypeColumn = "CharacterType"
+    Friend Const CompletionsColumn = "Completions"
     Friend Const EquipSlotColumn = "EquipSlot"
     Friend Const ItemIdColumn = "ItemId"
     Friend Const LocationIdColumn = "LocationId"
+    Friend Const QuestColumn = "Quest"
 End Module
