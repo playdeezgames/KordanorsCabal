fd6c9f63cd7d2f67c591ecebc68523da95c80dad
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-09-05 05:35:13 -0500

    ShouldRemoveAllSpellLevelsForACharacterFromTheStore

---
+++ src/KordanorsCabal.Data.Tests/Character/CharacterSpellDataTests.vb
@@ -0,0 +1,19 @@
+﻿Public Class CharacterSpellDataTests
+    Inherits WorldDataSubobjectTests(Of ICharacterSpellData)
+
+    Sub New()
+        MyBase.New(Function(x) x.CharacterSpell)
+    End Sub
+    <Fact>
+    Sub ShouldRemoveAllSpellLevelsForACharacterFromTheStore()
+        WithSubobject(
+            Sub(store, subject)
+                Dim characterId = 1L
+                subject.ClearForCharacter(characterId)
+                store.Verify(Sub(x) x.ClearForColumnValue(
+                                 It.IsAny(Of Action),
+                                 CharacterSpells,
+                                 (CharacterIdColumn, characterId)))
+            End Sub)
+    End Sub
+End Class
--- src/KordanorsCabal.Data/Interface/Character/ICharacterSpellData.vb
+++ src/KordanorsCabal.Data/Interface/Character/ICharacterSpellData.vb
@@ -1,6 +1,6 @@
 ﻿Public Interface ICharacterSpellData
-    Function Read(characterId As Long, spellType As Long) As Long?
-    Sub Write(characterId As Long, spellType As Long, level As Long)
-    Function ReadForCharacter(characterId As Long) As IEnumerable(Of Tuple(Of Long, Long))
     Sub ClearForCharacter(characterId As Long)
+    Function Read(characterId As Long, spellType As Long) As Long?
+    Function ReadForCharacter(characterId As Long) As IEnumerable(Of Tuple(Of Long, Long))
+    Sub Write(characterId As Long, spellType As Long, level As Long)
 End Interface
