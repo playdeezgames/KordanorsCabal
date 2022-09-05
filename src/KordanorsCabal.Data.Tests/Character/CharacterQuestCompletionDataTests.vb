2556e9bebe7466e3e55e3e1f83559a64a01b6858
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-09-05 04:57:42 -0500

    ShouldClearTheStoreOfAnyQuestCompletionsAssociatedWithACharacter

---
+++ src/KordanorsCabal.Data.Tests/Character/CharacterQuestCompletionDataTests.vb
@@ -0,0 +1,16 @@
+﻿Public Class CharacterQuestCompletionDataTests
+    Inherits WorldDataSubobjectTests
+    <Fact>
+    Sub ShouldClearTheStoreOfAnyQuestCompletionsAssociatedWithACharacter()
+        WithSubobject(
+            Function(x) x.CharacterQuestCompletion,
+            Sub(store, subject)
+                Dim characterId = 1L
+                subject.ClearForCharacter(characterId)
+                store.Verify(Sub(x) x.ClearForColumnValue(
+                                 It.IsAny(Of Action),
+                                 Tables.CharacterQuestCompletions,
+                                 (CharacterIdColumn, characterId)))
+            End Sub)
+    End Sub
+End Class
--- src/KordanorsCabal.Data/Interface/Character/ICharacterQuestCompletionData.vb
+++ src/KordanorsCabal.Data/Interface/Character/ICharacterQuestCompletionData.vb
@@ -1,5 +1,5 @@
 ﻿Public Interface ICharacterQuestCompletionData
     Sub ClearForCharacter(characterId As Long)
-    Sub Write(characterId As Long, questId As Long, count As Long)
     Function Read(characterId As Long, questId As Long) As Long?
+    Sub Write(characterId As Long, questId As Long, count As Long)
 End Interface
