7a40c95907382196af244b497c82d0d2e365ba40
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-09-05 04:40:03 -0500

    ShouldClearLocationsForACharacter

---
+++ src/KordanorsCabal.Data.Tests/CharacterLocationDataTests.vb
@@ -0,0 +1,16 @@
+﻿Public Class CharacterLocationDataTests
+    Inherits WorldDataSubobjectTests
+    <Fact>
+    Sub ShouldClearLocationsForACharacter()
+        WithSubobject(
+            Function(x) x.CharacterLocation,
+            Sub(store, subject)
+                Const characterId = 1L
+                subject.ClearForCharacter(characterId)
+                store.Verify(Sub(x) x.ClearForColumnValue(
+                                 It.IsAny(Of Action),
+                                 Tables.CharacterLocations,
+                                 (Columns.CharacterId, characterId)))
+            End Sub)
+    End Sub
+End Class
--- src/KordanorsCabal.Data/Interface/Character/ICharacterLocationData.vb
+++ src/KordanorsCabal.Data/Interface/Character/ICharacterLocationData.vb
@@ -1,5 +1,5 @@
 ﻿Public Interface ICharacterLocationData
     Sub ClearForCharacter(characterId As Long)
-    Sub Write(characterId As Long, locationId As Long)
     Function Read(characterId As Long, locationId As Long) As Boolean
+    Sub Write(characterId As Long, locationId As Long)
 End Interface
