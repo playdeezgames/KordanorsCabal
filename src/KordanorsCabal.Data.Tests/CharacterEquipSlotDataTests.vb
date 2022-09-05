6700f04c01a5bb4e9f27ca7e0f3ba9e5683a1845
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-09-05 04:12:20 -0500

    ShouldClearOutDataForACharactersEquipSlot

--- src/KordanorsCabal.Data.Tests/CharacterEquipSlotDataTests.vb
+++ src/KordanorsCabal.Data.Tests/CharacterEquipSlotDataTests.vb
@@ -1,5 +1,8 @@
 ﻿Public Class CharacterEquipSlotDataTests
-    Private Shared Sub WithCharacterEquipSlotData(stuffToDo As Action(Of Mock(Of IStore), ICharacterEquipSlotData))
+    Private Shared Sub WithCharacterEquipSlotData(
+                                                 stuffToDo As Action(Of
+                                                 Mock(Of IStore),
+                                                 ICharacterEquipSlotData))
         Dim store As New Mock(Of IStore)
         Dim worldData As New WorldData(store.Object)
         Dim subject = worldData.CharacterEquipSlot
@@ -12,7 +15,11 @@
             Sub(store, subject)
                 Const itemId = 1L
                 subject.ClearForItem(itemId)
-                store.Verify(Sub(x) x.ClearForColumnValue(It.IsAny(Of Action), Tables.CharacterEquipSlots, (Columns.ItemId, itemId)), Times.Once)
+                store.Verify(Sub(x) x.ClearForColumnValue(
+                             It.IsAny(Of Action),
+                             Tables.CharacterEquipSlots,
+                             (Columns.ItemId, itemId)),
+                             Times.Once)
             End Sub)
     End Sub
     <Fact>
@@ -21,7 +28,26 @@
             Sub(store, subject)
                 Const characterId = 1L
                 subject.ClearForCharacter(characterId)
-                store.Verify(Sub(x) x.ClearForColumnValue(It.IsAny(Of Action), Tables.CharacterEquipSlots, (Columns.CharacterId, characterId)), Times.Once)
+                store.Verify(Sub(x) x.ClearForColumnValue(
+                             It.IsAny(Of Action),
+                             Tables.CharacterEquipSlots,
+                             (Columns.CharacterId, characterId)),
+                             Times.Once)
+            End Sub)
+    End Sub
+    <Fact>
+    Public Sub ShouldClearOutDataForACharactersEquipSlot()
+        WithCharacterEquipSlotData(
+            Sub(store, subject)
+                Const characterId = 1L
+                Const equipSlot = 2L
+                subject.Clear(characterId, equipSlot)
+                store.Verify(Sub(x) x.ClearForColumnValues(
+                             It.IsAny(Of Action),
+                             Tables.CharacterEquipSlots,
+                             (Columns.CharacterId, characterId),
+                             (Columns.EquipSlot, equipSlot)),
+                             Times.Once)
             End Sub)
     End Sub
 End Class
--- src/KordanorsCabal.Data.Tests/Utility/Columns.vb
+++ src/KordanorsCabal.Data.Tests/Utility/Columns.vb
@@ -1,6 +1,7 @@
 ﻿Module Columns
     Friend Const CharacterId = "CharacterId"
     Friend Const CharacterType = "CharacterType"
+    Friend Const EquipSlot = "EquipSlot"
     Friend Const ItemId = "ItemId"
     Friend Const LocationId = "LocationId"
 End Module
