1ef9f76315b32f226e436802357e32de88d97d2d
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-09-06 04:39:45 -0500

    ShouldQueryTheDataStoreToFindACharactersMoneyDropDice

--- src/KordanorsCabal.Data.Tests/Character/CharacterTypeDataTests.vb
+++ src/KordanorsCabal.Data.Tests/Character/CharacterTypeDataTests.vb
@@ -14,4 +14,30 @@
                                  Columns.CharacterTypeIdColumn))
             End Sub)
     End Sub
+    <Fact>
+    Public Sub ShouldQueryTheDataStoreToDetermineWhetherOrNotACharacterTypeIsUndead()
+        WithSubobject(
+            Sub(store, subject)
+                Dim characterType = 1L
+                subject.ReadIsUndead(characterType).ShouldBeNull
+                store.Verify(Function(x) x.ReadColumnValue(Of Long, Long)(
+                                 It.IsAny(Of Action),
+                                 Tables.CharacterTypes,
+                                 Columns.IsUndeadColumn,
+                                 (Columns.CharacterTypeIdColumn, characterType)))
+            End Sub)
+    End Sub
+    <Fact>
+    Public Sub ShouldQueryTheDataStoreToFindACharactersMoneyDropDice()
+        WithSubobject(
+            Sub(store, subject)
+                Dim characterType = 1L
+                subject.ReadMoneyDropDice(characterType).ShouldBeNull
+                store.Verify(Function(x) x.ReadColumnString(Of Long)(
+                                 It.IsAny(Of Action),
+                                 Tables.CharacterTypes,
+                                 Columns.MoneyDropDiceColumn,
+                                 (Columns.CharacterTypeIdColumn, characterType)))
+            End Sub)
+    End Sub
 End Class
