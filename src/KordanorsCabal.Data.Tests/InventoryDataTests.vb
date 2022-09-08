45fe37ff4746f08d2faa8cf1e4039c6f7ffb3c3f
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-09-08 04:54:19 -0500

    ShouldClearDataForAGivenCharacter

---
+++ src/KordanorsCabal.Data.Tests/InventoryDataTests.vb
@@ -0,0 +1,19 @@
+﻿Public Class InventoryDataTests
+    Inherits WorldDataSubobjectTests(Of IInventoryData)
+    Sub New()
+        MyBase.New(Function(x) x.Inventory)
+    End Sub
+    <Fact>
+    Sub ShouldClearDataForAGivenCharacter()
+        WithSubobject(
+            Sub(store, subject)
+                Dim characterId = 1L
+                subject.ClearForCharacter(characterId)
+                store.Verify(
+                Sub(x) x.ClearForColumnValue(
+                    It.IsAny(Of Action),
+                    Tables.Inventories,
+                    (Columns.CharacterIdColumn, characterId)))
+            End Sub)
+    End Sub
+End Class
--- src/KordanorsCabal.Data/Interface/Item/IInventoryData.vb
+++ src/KordanorsCabal.Data/Interface/Item/IInventoryData.vb
@@ -1,7 +1,7 @@
 ﻿Public Interface IInventoryData
     Sub ClearForCharacter(characterId As Long)
-    Function ReadForLocation(locationId As Long) As Long?
+    Function CreateForCharacter(characterId As Long) As Long
     Function CreateForLocation(locationId As Long) As Long
     Function ReadForCharacter(characterId As Long) As Long?
-    Function CreateForCharacter(characterId As Long) As Long
+    Function ReadForLocation(locationId As Long) As Long?
 End Interface
