0eaa6f2683ca9be9182919baa800e11e1c57c69f
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-09-08 04:55:37 -0500

    ShouldCreateInventoryForAGivenCharacter

--- src/KordanorsCabal.Data.Tests/InventoryDataTests.vb
+++ src/KordanorsCabal.Data.Tests/InventoryDataTests.vb
@@ -4,13 +4,26 @@
         MyBase.New(Function(x) x.Inventory)
     End Sub
     <Fact>
-    Sub ShouldClearDataForAGivenCharacter()
+    Sub ShouldClearInventoryDataForAGivenCharacter()
         WithSubobject(
             Sub(store, subject)
                 Dim characterId = 1L
                 subject.ClearForCharacter(characterId)
                 store.Verify(
                 Sub(x) x.ClearForColumnValue(
+                    It.IsAny(Of Action),
+                    Tables.Inventories,
+                    (Columns.CharacterIdColumn, characterId)))
+            End Sub)
+    End Sub
+    <Fact>
+    Sub ShouldCreateInventoryForAGivenCharacter()
+        WithSubobject(
+            Sub(store, subject)
+                Dim characterId = 1L
+                subject.CreateForCharacter(characterId).ShouldBe(0)
+                store.Verify(
+                Sub(x) x.CreateRecord(
                     It.IsAny(Of Action),
                     Tables.Inventories,
                     (Columns.CharacterIdColumn, characterId)))
