3eea2815b05ec7037362dea85963e78e87f9e00c
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-09-09 05:36:41 -0500

    ShouldUpdateTheStoreWithAStatisticValueForAGivenCombinationOfLocationAndStatisticType

--- src/KordanorsCabal.Data.Tests/Location/LocationStatisticDataTests.vb
+++ src/KordanorsCabal.Data.Tests/Location/LocationStatisticDataTests.vb
@@ -19,4 +19,21 @@
                     (Columns.LocationStatisticTypeIdColumn, statisticType)))
             End Sub)
     End Sub
+    <Fact>
+    Sub ShouldUpdateTheStoreWithAStatisticValueForAGivenCombinationOfLocationAndStatisticType()
+        WithSubobject(
+            Sub(store, subject)
+                Dim locationId = 1L
+                Dim statisticType = 2L
+                Dim statisticValue = 3L
+                subject.Write(locationId, statisticType, statisticValue)
+                store.Verify(
+                    Sub(x) x.ReplaceRecord(
+                    It.IsAny(Of Action),
+                    Tables.LocationStatistics,
+                    (Columns.LocationIdColumn, locationId),
+                    (Columns.LocationStatisticTypeIdColumn, statisticType),
+                    (Columns.StatisticValueColumn, statisticValue)))
+            End Sub)
+    End Sub
 End Class
