330d3044f3fe848af8ccd199ebd5a38d2f36b52d
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-09-09 05:41:41 -0500

    ShouldClearTheStoreOfPlayerDataAssociatedWithAGivenCharacter

---
+++ src/KordanorsCabal.Data.Tests/Character/PlayerDataTests.vb
@@ -0,0 +1,19 @@
+﻿Public Class PlayerDataTests
+    Inherits WorldDataSubobjectTests(Of IPlayerData)
+    Sub New()
+        MyBase.New(Function(x) x.Player)
+    End Sub
+    <Fact>
+    Sub ShouldClearTheStoreOfPlayerDataAssociatedWithAGivenCharacter()
+        WithSubobject(
+            Sub(store, subject)
+                Dim characterId = 1L
+                subject.ClearForCharacter(characterId)
+                store.Verify(
+                    Sub(x) x.ClearForColumnValue(
+                    It.IsAny(Of Action),
+                    Tables.Players,
+                    (Columns.CharacterIdColumn, characterId)))
+            End Sub)
+    End Sub
+End Class
