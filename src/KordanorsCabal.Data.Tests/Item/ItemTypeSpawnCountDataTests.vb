2f3788f7b96aefaac5e2292a30437563d6cb6065
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-09-09 04:20:28 -0500

    remoive comment

--- src/KordanorsCabal.Data.Tests/Item/ItemTypeSpawnCountDataTests.vb
+++ src/KordanorsCabal.Data.Tests/Item/ItemTypeSpawnCountDataTests.vb
@@ -10,7 +10,6 @@
                 Dim itemTypeId = 1L
                 Dim dungeonLevelId = 2L
                 subject.Read(itemTypeId, dungeonLevelId).ShouldBeNull
-                'IStore.ReadColumnString<long, long>(Action, "ItemTypeSpawnCounts", "SpawnDice", (ItemTypeId, 1), (DungeonLevelId, 2))
                 store.Verify(
                     Function(x) x.ReadColumnString(Of Long, Long)(
                     It.IsAny(Of Action),
