b14df4d4cfee87673d2b0dd5c57a0147fdf846c9
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-09-10 05:40:13 -0500

    rearrange

--- src/KordanorsCabal.Game/Character/ICharacter.vb
+++ src/KordanorsCabal.Game/Character/ICharacter.vb
@@ -1,30 +1,31 @@
 ﻿Public Interface ICharacter
     Inherits IBaseThingie
+    ReadOnly Property CanIntimidate As Boolean
+    ReadOnly Property CharacterType As ICharacterType
+    Property CurrentHP As Long
+    ReadOnly Property IsDead As Boolean
+    ReadOnly Property IsUndead As Boolean
     Property Location As Location
+    ReadOnly Property MaximumHP As Long
+    ReadOnly Property Name As String
+    ReadOnly Property PartingShot As String
+
     Sub SetStatistic(statisticType As CharacterStatisticType, statisticValue As Long)
     Function GetStatistic(statisticType As CharacterStatisticType) As Long?
     Function IsEnemy(character As Character) As Boolean
     Function RollWillpower() As Long
-    ReadOnly Property Name As String
     Function RollDefend() As Long
     Function Kill(killedBy As Character) As (Sfx?, List(Of String))
     Function IsDemoralized() As Boolean
-    ReadOnly Property IsDead As Boolean
     Sub DoDamage(damage As Long)
     Function DoArmorWear(wear As Long) As IEnumerable(Of OldItemType)
     Sub Destroy()
-    Property CurrentHP As Long
-    ReadOnly Property CharacterType As ICharacterType
-    ReadOnly Property CanIntimidate As Boolean
     Sub AddStress(delta As Long)
-    ReadOnly Property IsUndead As Boolean
     Function CanBeBribedWith(itemType As OldItemType) As Boolean
     Sub DoImmobilization(delta As Long)
     Sub ChangeStatistic(statisticType As CharacterStatisticType, delta As Long)
     Function RollAttack() As Long
     Function RollInfluence() As Long
-    ReadOnly Property PartingShot As String
     Function DoWeaponWear(wear As Long) As IEnumerable(Of OldItemType)
     Function DetermineDamage(value As Long) As Long
-    ReadOnly Property MaximumHP As Long
 End Interface
