75119883fc02c87ae98c028781777550097b961b
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-09-10 05:47:27 -0500

    rearrange contract

--- src/KordanorsCabal.Game/Character/ICharacter.vb
+++ src/KordanorsCabal.Game/Character/ICharacter.vb
@@ -1,5 +1,6 @@
 ﻿Public Interface ICharacter
     Inherits IBaseThingie
+    Function CanBeBribedWith(itemType As OldItemType) As Boolean
     ReadOnly Property CanIntimidate As Boolean
     ReadOnly Property CharacterType As ICharacterType
     Property CurrentHP As Long
@@ -14,19 +15,22 @@
 
     Sub SetStatistic(statisticType As CharacterStatisticType, statisticValue As Long)
     Function GetStatistic(statisticType As CharacterStatisticType) As Long?
-    Function RollWillpower() As Long
-    Function RollDefend() As Long
+    Sub ChangeStatistic(statisticType As CharacterStatisticType, delta As Long)
+
     Function Kill(killedBy As ICharacter) As (Sfx?, List(Of String))
+    Sub Destroy()
+
+    Sub AddStress(delta As Long)
+    Function AddXP(xp As Long) As Boolean
+
+    Function DetermineDamage(value As Long) As Long
     Sub DoDamage(damage As Long)
     Function DoArmorWear(wear As Long) As IEnumerable(Of OldItemType)
-    Sub Destroy()
-    Sub AddStress(delta As Long)
-    Function CanBeBribedWith(itemType As OldItemType) As Boolean
     Sub DoImmobilization(delta As Long)
-    Sub ChangeStatistic(statisticType As CharacterStatisticType, delta As Long)
+    Function DoWeaponWear(wear As Long) As IEnumerable(Of OldItemType)
+
+    Function RollWillpower() As Long
+    Function RollDefend() As Long
     Function RollAttack() As Long
     Function RollInfluence() As Long
-    Function DoWeaponWear(wear As Long) As IEnumerable(Of OldItemType)
-    Function DetermineDamage(value As Long) As Long
-    Function AddXP(xp As Long) As Boolean
 End Interface
