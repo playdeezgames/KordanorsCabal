329b55e253f0f91b6b80ac6034baeb8c32627b85
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-09-12 14:19:40 -0500

    formatting

--- src/KordanorsCabal.Game.Tests/CharacterTests.vb
+++ src/KordanorsCabal.Game.Tests/CharacterTests.vb
@@ -11,28 +11,33 @@
                 Const questId = 1L
                 Const locationTypeId = 7L
                 Const characterTypeId = 13L
+
+                Dim locationData = New Mock(Of ILocationData)
+
                 worldData.SetupGet(Function(x) x.CharacterQuest).Returns((New Mock(Of ICharacterQuestData)).Object)
                 worldData.SetupGet(Function(x) x.CharacterQuestCompletion).Returns((New Mock(Of ICharacterQuestCompletionData)).Object)
-                Dim locationData = New Mock(Of ILocationData)
                 worldData.SetupGet(Function(x) x.Location).Returns(locationData.Object)
                 locationData.Setup(Function(x) x.ReadForLocationType(It.IsAny(Of Long))).Returns(New List(Of Long) From {locationId})
                 worldData.SetupGet(Function(x) x.CharacterTypeInitialStatistic).Returns((New Mock(Of ICharacterTypeInitialStatisticData)).Object)
                 worldData.SetupGet(Function(x) x.Character).Returns((New Mock(Of ICharacterData)).Object)
+
                 subject.AcceptQuest(Quest.CellarRats)
+
                 worldData.Verify(Function(x) x.CharacterQuest.Read(id, questId))
                 worldData.Verify(Sub(x) x.CharacterQuest.Write(id, questId))
                 worldData.Verify(Sub(x) x.CharacterQuestCompletion.Read(id, questId))
                 worldData.Verify(Function(x) x.Location.ReadForLocationType(locationTypeId))
                 worldData.Verify(Function(x) x.CharacterTypeInitialStatistic.ReadAllForCharacterType(characterTypeId))
                 worldData.Verify(Function(x) x.Character.Create(characterTypeId, locationId))
+                locationData.VerifyNoOtherCalls()
             End Sub)
     End Sub
     <Fact>
     Sub ShouldAttemptToChangeValuesAssociatedWithCurrentMP()
         WithAnySubject(
             Sub(id, worldData, subject)
-                Dim stress = 2L
-                Dim statisticTypeId = 13L
+                Const stress = 2L
+                Const statisticTypeId = 13L
                 worldData.SetupGet(Function(x) x.CharacterStatistic).Returns((New Mock(Of ICharacterStatisticData)).Object)
                 worldData.SetupGet(Function(x) x.CharacterStatisticType).Returns((New Mock(Of ICharacterStatisticTypeData)).Object)
                 subject.AddStress(stress)
@@ -44,9 +49,9 @@
     Sub ShouldAttemptToAddXP()
         WithAnySubject(
             Sub(id, worldData, subject)
-                Dim xp = 2L
-                Dim statisticTypeId = 16L
-                Dim otherStatisticTypeId = 17L
+                Const xp = 2L
+                Const statisticTypeId = 16L
+                Const otherStatisticTypeId = 17L
                 worldData.SetupGet(Function(x) x.CharacterStatistic).Returns((New Mock(Of ICharacterStatisticData)).Object)
                 worldData.SetupGet(Function(x) x.CharacterStatisticType).Returns((New Mock(Of ICharacterStatisticTypeData)).Object)
                 subject.AddXP(xp).ShouldBeFalse
@@ -60,11 +65,14 @@
     Sub ShouldAttemptToAssignPoints()
         WithAnySubject(
             Sub(id, worldData, subject)
-                Dim statisticTypeId = 9
+                Const statisticTypeId = 9
+
+                Dim statisticType As New Mock(Of ICharacterStatisticType)
                 worldData.Setup(Function(x) x.CharacterStatistic).Returns((New Mock(Of ICharacterStatisticData)).Object)
                 worldData.Setup(Function(x) x.CharacterStatisticType).Returns((New Mock(Of ICharacterStatisticTypeData)).Object)
-                Dim statisticType As New Mock(Of ICharacterStatisticType)
+
                 subject.AssignPoint(statisticType.Object)
+
                 statisticType.VerifyNoOtherCalls()
                 worldData.Verify(Function(x) x.CharacterStatistic.Read(id, statisticTypeId))
                 worldData.Verify(Function(x) x.CharacterStatisticType.ReadDefaultValue(statisticTypeId))
@@ -76,8 +84,11 @@
             Sub(id, worldData, subject)
                 Const quest = Game.Quest.CellarRats
                 Const questId = CType(quest, Long)
+
                 worldData.SetupGet(Function(x) x.CharacterQuest).Returns((New Mock(Of ICharacterQuestData)).Object)
+
                 subject.CanAcceptQuest(quest).ShouldBeTrue
+
                 worldData.Verify(Function(x) x.CharacterQuest.Read(id, questId))
             End Sub)
     End Sub
@@ -86,28 +97,34 @@
         WithAnySubject(
             Sub(id, worldData, subject)
                 Const characterTypeId = 2L
-                Dim itemType = OldItemType.AirShard
-                Dim itemTypeId = CType(itemType, Long)
+                Const itemType = OldItemType.AirShard
+                Const itemTypeId = CType(itemType, Long)
+
                 Dim characterData = New Mock(Of ICharacterData)
                 characterData.Setup(Function(x) x.ReadCharacterType(It.IsAny(Of Long))).Returns(characterTypeId)
                 worldData.SetupGet(Function(x) x.Character).Returns(characterData.Object)
                 worldData.SetupGet(Function(X) X.CharacterTypeBribe).Returns((New Mock(Of ICharacterTypeBribeData)).Object)
+
                 subject.CanBeBribedWith(itemType).ShouldBeFalse
+
                 characterData.Verify(Function(x) x.ReadCharacterType(id))
                 worldData.Verify(Function(x) x.CharacterTypeBribe.Read(characterTypeId, itemTypeId))
+                characterData.VerifyNoOtherCalls()
             End Sub)
     End Sub
     <Fact>
     Sub ShouldDetermineWhetherAGiveCharacterCanCastAGivenSpell()
         WithAnySubject(
             Sub(id, worldData, subject)
-                Dim spellType = Game.SpellType.Purify
-                Dim spellTypeId = CType(spellType, Long)
-                Dim statisticTypeId = 8L
-                Dim otherStatisticTypeId = 15L
+                Const spellType = Game.SpellType.Purify
+                Const statisticTypeId = 8L
+                Const otherStatisticTypeId = 15L
+
                 worldData.SetupGet(Function(x) x.CharacterStatistic).Returns((New Mock(Of ICharacterStatisticData)).Object)
                 worldData.SetupGet(Function(x) x.CharacterStatisticType).Returns((New Mock(Of ICharacterStatisticTypeData)).Object)
+
                 subject.CanCastSpell(spellType).ShouldBeFalse
+
                 worldData.Verify(Function(x) x.CharacterStatistic.Read(id, statisticTypeId))
                 worldData.Verify(Function(x) x.CharacterStatistic.Read(id, otherStatisticTypeId))
                 worldData.Verify(Function(x) x.CharacterStatisticType.ReadDefaultValue(statisticTypeId))
