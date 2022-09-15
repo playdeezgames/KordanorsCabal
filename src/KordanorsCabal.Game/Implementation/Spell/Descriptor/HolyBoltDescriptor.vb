be089e7fd5142fecf03441885cacaa599cf8d91c
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-09-15 04:40:20 -0500

    eliminate spell type descriptors

--- src/KordanorsCabal.Game/Implementation/Spell/Descriptor/HolyBoltDescriptor.vb
+++
@@ -1,7 +0,0 @@
-﻿Friend Class HolyBoltDescriptor
-    Inherits SpellType
-
-    Public Sub New(worldData As IWorldData, id As Long)
-        MyBase.New(worldData, id)
-    End Sub
-End Class
--- src/KordanorsCabal.Game/Implementation/Spell/Descriptor/PurifyDescriptor.vb
+++
@@ -1,7 +0,0 @@
-﻿Friend Class PurifyDescriptor
-    Inherits SpellType
-
-    Public Sub New(worldData As IWorldData, id As Long)
-        MyBase.New(worldData, id)
-    End Sub
-End Class
