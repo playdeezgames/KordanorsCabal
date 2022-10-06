679364d4c7de53668493d25bb07de582fd227218
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-10-06 05:07:28 -0500

    ItemTypeExtensions

--- src/KordanorsCabal.Game/Implementation/Item/OldItemType.vb
+++ src/KordanorsCabal.Game/Implementation/Item/OldItemType.vb
@@ -3,9 +3,3 @@
 Public Enum OldItemType
     None
 End Enum
-Public Module ItemTypeExtensions
-    <Extension>
-    Function ToNew(itemType As OldItemType, worldData As IWorldData) As IItemType
-        Return Game.ItemType.FromId(worldData, itemType)
-    End Function
-End Module
