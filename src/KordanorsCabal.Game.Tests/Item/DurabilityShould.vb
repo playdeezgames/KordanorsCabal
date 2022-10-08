c3908901d9ec18d49d187b98e4870ca284e9bd49
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-10-08 04:37:48 -0500

    durabilityshould became a thingishould

--- src/KordanorsCabal.Game.Tests/Item/DurabilityShould.vb
+++ src/KordanorsCabal.Game.Tests/Item/DurabilityShould.vb
@@ -1,64 +1,63 @@
 ﻿Public Class DurabilityShould
-    Private Sub WithSubject(stuffToDo As Action(Of Mock(Of IWorldData), Long, IDurability))
-        Const itemId = 1L
-        Dim worldData As New Mock(Of IWorldData)
-        Dim durability As IDurability = Game.Durability.FromId(worldData.Object, itemId)
-        stuffToDo(worldData, itemId, durability)
-        worldData.VerifyNoOtherCalls()
+    Inherits ThingieShould(Of IDurability)
+
+    Public Sub New()
+        MyBase.New(AddressOf Durability.FromId)
     End Sub
+
     <Fact>
     Sub have_durability()
         WithSubject(
-            Sub(worldData, itemId, item)
+            Sub(worldData, id, subject)
                 Const itemTypeId = 2L
                 worldData.Setup(Function(x) x.Item.ReadItemType(It.IsAny(Of Long))).Returns(itemTypeId)
                 worldData.Setup(Function(x) x.ItemTypeStatistic.Read(It.IsAny(Of Long), It.IsAny(Of Long))).Returns(1L)
                 worldData.Setup(Function(x) x.ItemStatistic.Read(It.IsAny(Of Long), It.IsAny(Of Long)))
-                item.Current.ShouldBe(1L)
-                worldData.Verify(Function(x) x.Item.ReadItemType(itemId))
+                subject.Current.ShouldBe(1L)
+                worldData.Verify(Function(x) x.Item.ReadItemType(id))
                 worldData.Verify(Function(x) x.ItemTypeStatistic.Read(itemTypeId, 5L))
-                worldData.Verify(Function(x) x.ItemStatistic.Read(itemId, 1L))
+                worldData.Verify(Function(x) x.ItemStatistic.Read(id, 1L))
             End Sub)
     End Sub
     <Fact>
     Sub have_maximum_durability()
         WithSubject(
-            Sub(worldData, itemId, item)
+            Sub(worldData, id, subject)
                 Const itemTypeId = 2L
                 worldData.Setup(Function(x) x.Item.ReadItemType(It.IsAny(Of Long))).Returns(itemTypeId)
                 worldData.Setup(Function(x) x.ItemTypeStatistic.Read(It.IsAny(Of Long), It.IsAny(Of Long)))
-                item.Maximum.ShouldBeNull
-                worldData.Verify(Function(x) x.Item.ReadItemType(itemId))
+                subject.Maximum.ShouldBeNull
+                worldData.Verify(Function(x) x.Item.ReadItemType(id))
                 worldData.Verify(Function(x) x.ItemTypeStatistic.Read(itemTypeId, 5L))
             End Sub)
     End Sub
     <Fact>
     Sub reduce_durability()
         WithSubject(
-            Sub(worldData, itemId, item)
+            Sub(worldData, id, subject)
                 Const itemTypeId = 2L
                 worldData.Setup(Function(x) x.Item.ReadItemType(It.IsAny(Of Long))).Returns(itemTypeId)
                 worldData.Setup(Function(x) x.ItemTypeStatistic.Read(It.IsAny(Of Long), It.IsAny(Of Long))).Returns(1L)
                 worldData.Setup(Function(x) x.ItemStatistic.Read(It.IsAny(Of Long), It.IsAny(Of Long)))
-                item.Reduce(1L)
-                worldData.Verify(Function(x) x.Item.ReadItemType(itemId))
+                subject.Reduce(1L)
+                worldData.Verify(Function(x) x.Item.ReadItemType(id))
                 worldData.Verify(Function(x) x.ItemTypeStatistic.Read(itemTypeId, 5L))
-                worldData.Verify(Function(x) x.ItemStatistic.Read(itemId, 1L))
-                worldData.Verify(Sub(x) x.ItemStatistic.Write(itemId, 1L, 1L))
+                worldData.Verify(Function(x) x.ItemStatistic.Read(id, 1L))
+                worldData.Verify(Sub(x) x.ItemStatistic.Write(id, 1L, 1L))
             End Sub)
     End Sub
     <Fact>
     Sub have_is_broken_flag()
         WithSubject(
-            Sub(worldData, itemId, item)
+            Sub(worldData, id, subject)
                 Const itemTypeId = 2L
                 worldData.Setup(Function(x) x.Item.ReadItemType(It.IsAny(Of Long))).Returns(itemTypeId)
                 worldData.Setup(Function(x) x.ItemTypeStatistic.Read(It.IsAny(Of Long), It.IsAny(Of Long))).Returns(1L)
                 worldData.Setup(Function(x) x.ItemStatistic.Read(It.IsAny(Of Long), It.IsAny(Of Long)))
-                item.IsBroken.ShouldBeFalse
-                worldData.Verify(Function(x) x.Item.ReadItemType(itemId))
+                subject.IsBroken.ShouldBeFalse
+                worldData.Verify(Function(x) x.Item.ReadItemType(id))
                 worldData.Verify(Function(x) x.ItemTypeStatistic.Read(itemTypeId, 5L))
-                worldData.Verify(Function(x) x.ItemStatistic.Read(itemId, 1L))
+                worldData.Verify(Function(x) x.ItemStatistic.Read(id, 1L))
             End Sub)
     End Sub
 End Class
