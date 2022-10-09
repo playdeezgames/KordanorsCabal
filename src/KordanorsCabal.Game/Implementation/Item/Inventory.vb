bff9780e81d2501e6c7ceeed1b37a39f5eff150a
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-10-09 05:38:27 -0500

    removed whitespace

--- src/KordanorsCabal.Game/Implementation/Item/Inventory.vb
+++ src/KordanorsCabal.Game/Implementation/Item/Inventory.vb
@@ -12,13 +12,11 @@
             Return Not Items.Any
         End Get
     End Property
-
     ReadOnly Property Items As IReadOnlyList(Of IItem) Implements IInventory.Items
         Get
             Return WorldData.InventoryItem.ReadItems(Id).Select(Function(x) Item.FromId(WorldData, x)).ToList
         End Get
     End Property
-
     Public Sub Add(item As IItem) Implements IInventory.Add
         WorldData.InventoryItem.Write(Id, item.Id)
     End Sub
@@ -27,7 +25,6 @@
             Return Items.Where(Function(x) x.ItemType.Id = itemType.Id)
         End Get
     End Property
-
     ReadOnly Property TotalEncumbrance As Long Implements IInventory.TotalEncumbrance
         Get
             Return Items.Sum(Function(x) x.ItemType.Encumbrance)
--- src/KordanorsCabal.Game/Implementation/Item/Item.vb
+++ src/KordanorsCabal.Game/Implementation/Item/Item.vb
@@ -20,7 +20,6 @@
             Return ItemType.Name
         End Get
     End Property
-
     Public Sub Purify() Implements IItem.Purify
         ItemType.Purify.Invoke(Me)
     End Sub
@@ -28,47 +27,31 @@
     Public Sub Destroy() Implements IItem.Destroy
         WorldData.Item.Clear(Id)
     End Sub
-    Private Sub ChangeStatistic(statisticType As ItemStatisticType, delta As Long)
-        SetStatistic(statisticType, GetStatistic(statisticType) + delta)
-    End Sub
-
-    Private Function GetStatistic(statisticType As ItemStatisticType) As Long
-        Return If(WorldData.ItemStatistic.Read(Id, statisticType), statisticType.DefaultValue)
-    End Function
-    Private Sub SetStatistic(statisticType As ItemStatisticType, value As Long)
-        WorldData.ItemStatistic.Write(Id, statisticType, value)
-    End Sub
-
     Public ReadOnly Property Weapon As IWeapon Implements IItem.Weapon
         Get
             Return Game.Weapon.FromId(WorldData, Id)
         End Get
     End Property
-
     Public ReadOnly Property Durability As IDurability Implements IItem.Durability
         Get
             Return Game.Durability.FromId(WorldData, Id)
         End Get
     End Property
-
     Public ReadOnly Property Repair As IRepair Implements IItem.Repair
         Get
             Return Game.Repair.FromId(WorldData, Id)
         End Get
     End Property
-
     Public ReadOnly Property Armor As IArmor Implements IItem.Armor
         Get
             Return Game.Armor.FromId(WorldData, Id)
         End Get
     End Property
-
     Public ReadOnly Property Equipment As IEquipment Implements IItem.Equipment
         Get
             Return Game.Equipment.FromId(WorldData, Id)
         End Get
     End Property
-
     Public ReadOnly Property Usage As IUsage Implements IItem.Usage
         Get
             Return Game.Usage.FromId(WorldData, Id)
