e3002f343cbf150b79850e299d98713326558fdb
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-10-10 11:14:30 -0500

    casing

--- src/KordanorsCabal.Data.Tests/WorldDataTests.vb
+++ src/KordanorsCabal.Data.Tests/WorldDataTests.vb
@@ -1,113 +1,112 @@
-Namespace KordanorsCabal.Data.Tests
-    Public Class WorldDataTests
-        <Fact>
-        Sub ShouldConstructAllOfTheDataSubobjects()
-            Dim store As New Mock(Of IStore)
-            Dim checker As New Mock(Of IEventData)
-            Dim subject As IWorldData = New WorldData(store.Object, checker.Object)
-            subject.Character.ShouldNotBeNull
-            subject.CharacterEquipSlot.ShouldNotBeNull
-            subject.CharacterLocation.ShouldNotBeNull
-            subject.CharacterQuestCompletion.ShouldNotBeNull
-            subject.CharacterQuest.ShouldNotBeNull
-            subject.CharacterSpell.ShouldNotBeNull
-            subject.CharacterStatistic.ShouldNotBeNull
-            subject.CharacterStatisticType.ShouldNotBeNull
-            subject.CharacterType.ShouldNotBeNull
-            subject.CharacterTypeAttackType.ShouldNotBeNull
-            subject.CharacterTypeBribe.ShouldNotBeNull
-            subject.CharacterTypeEnemy.ShouldNotBeNull
-            subject.CharacterTypeInitialStatistic.ShouldNotBeNull
-            subject.CharacterTypeLoot.ShouldNotBeNull
-            subject.CharacterTypePartingShot.ShouldNotBeNull
-            subject.CharacterTypeSpawnCount.ShouldNotBeNull
-            subject.CharacterTypeSpawnLocation.ShouldNotBeNull
-            subject.Direction.ShouldNotBeNull
-            subject.DungeonLevel.ShouldNotBeNull
-            subject.EquipSlot.ShouldNotBeNull
-            subject.Feature.ShouldNotBeNull
-            subject.FeatureType.ShouldNotBeNull
-            subject.Inventory.ShouldNotBeNull
-            subject.InventoryItem.ShouldNotBeNull
-            subject.Item.ShouldNotBeNull
-            subject.ItemStatistic.ShouldNotBeNull
-            subject.ItemType.ShouldNotBeNull
-            subject.ItemTypeCharacterStatisticBuff.ShouldNotBeNull
-            subject.ItemTypeEquipSlot.ShouldNotBeNull
-            subject.ItemTypeEvent.ShouldNotBeNull
-            subject.ItemTypeShopType.ShouldNotBeNull
-            subject.ItemTypeSpawnCount.ShouldNotBeNull
-            subject.ItemTypeSpawnLocationType.ShouldNotBeNull
-            subject.ItemTypeStatistic.ShouldNotBeNull
-            subject.ItemTypeStatisticType.ShouldNotBeNull
-            subject.Location.ShouldNotBeNull
-            subject.LocationDungeonLevel.ShouldNotBeNull
-            subject.LocationStatistic.ShouldNotBeNull
-            subject.LocationType.ShouldNotBeNull
-            subject.Player.ShouldNotBeNull
-            subject.QuestType.ShouldNotBeNull
-            subject.Route.ShouldNotBeNull
-            subject.RouteType.ShouldNotBeNull
-            subject.RouteTypeLock.ShouldNotBeNull
-            subject.ShoppeType.ShouldNotBeNull
-            subject.SpellType.ShouldNotBeNull
-            subject.SpellTypeRequiredPower.ShouldNotBeNull
-            subject.Events.ShouldNotBeNull
-            store.VerifyNoOtherCalls()
-            checker.VerifyNoOtherCalls()
-        End Sub
-        <Fact>
-        Sub ShouldSaveToTheStore()
-            Dim store As New Mock(Of IStore)
-            Dim checker As New Mock(Of IEventData)
-            Dim subject As IWorldData = New WorldData(store.Object, checker.Object)
-            Const filename = "filename"
-            subject.Save(filename)
-            store.Verify(Sub(x) x.Save(filename), Times.Once)
-            store.VerifyNoOtherCalls()
-            Checker.VerifyNoOtherCalls()
-        End Sub
-        <Fact>
-        Sub ShouldLoadFromTheStore()
-            Dim store As New Mock(Of IStore)
-            Dim checker As New Mock(Of IEventData)
-            Dim subject As IWorldData = New WorldData(store.Object, checker.Object)
-            Const filename = "filename"
-            subject.Load(filename)
-            store.Verify(Sub(x) x.Load(filename), Times.Once)
-            store.VerifyNoOtherCalls()
-            Checker.VerifyNoOtherCalls()
-        End Sub
-        <Fact>
-        Sub ShouldResetTheStore()
-            Dim store As New Mock(Of IStore)
-            Dim checker As New Mock(Of IEventData)
-            Dim subject As IWorldData = New WorldData(store.Object, checker.Object)
-            subject.Reset()
-            store.Verify(Sub(x) x.Reset(), Times.Once)
-            store.VerifyNoOtherCalls()
-            Checker.VerifyNoOtherCalls()
-        End Sub
-        <Fact>
-        Sub ShouldRenewTheStore()
-            Dim store As New Mock(Of IStore)
-            Dim checker As New Mock(Of IEventData)
-            Dim subject As IWorldData = New WorldData(store.Object, checker.Object)
-            subject.Renew().ShouldBeNull
-            store.Verify(Function(x) x.Renew(), Times.Once)
-            store.VerifyNoOtherCalls()
-            Checker.VerifyNoOtherCalls()
-        End Sub
-        <Fact>
-        Sub ShouldRestoreTheStore()
-            Dim store As New Mock(Of IStore)
-            Dim checker As New Mock(Of IEventData)
-            Dim subject As IWorldData = New WorldData(store.Object, checker.Object)
-            subject.Restore(Nothing)
-            store.Verify(Sub(x) x.Restore(Nothing), Times.Once)
-            store.VerifyNoOtherCalls()
-            Checker.VerifyNoOtherCalls()
-        End Sub
-    End Class
-End Namespace
+Public Class WorldDataTests
+    <Fact>
+    Sub ShouldConstructAllOfTheDataSubobjects()
+        Dim store As New Mock(Of IStore)
+        Dim checker As New Mock(Of IEventData)
+        Dim subject As IWorldData = New WorldData(store.Object, checker.Object)
+        subject.Character.ShouldNotBeNull
+        subject.CharacterEquipSlot.ShouldNotBeNull
+        subject.CharacterLocation.ShouldNotBeNull
+        subject.CharacterQuestCompletion.ShouldNotBeNull
+        subject.CharacterQuest.ShouldNotBeNull
+        subject.CharacterSpell.ShouldNotBeNull
+        subject.CharacterStatistic.ShouldNotBeNull
+        subject.CharacterStatisticType.ShouldNotBeNull
+        subject.CharacterType.ShouldNotBeNull
+        subject.CharacterTypeAttackType.ShouldNotBeNull
+        subject.CharacterTypeBribe.ShouldNotBeNull
+        subject.CharacterTypeEnemy.ShouldNotBeNull
+        subject.CharacterTypeInitialStatistic.ShouldNotBeNull
+        subject.CharacterTypeLoot.ShouldNotBeNull
+        subject.CharacterTypePartingShot.ShouldNotBeNull
+        subject.CharacterTypeSpawnCount.ShouldNotBeNull
+        subject.CharacterTypeSpawnLocation.ShouldNotBeNull
+        subject.Direction.ShouldNotBeNull
+        subject.DungeonLevel.ShouldNotBeNull
+        subject.EquipSlot.ShouldNotBeNull
+        subject.Feature.ShouldNotBeNull
+        subject.FeatureType.ShouldNotBeNull
+        subject.Inventory.ShouldNotBeNull
+        subject.InventoryItem.ShouldNotBeNull
+        subject.Item.ShouldNotBeNull
+        subject.ItemStatistic.ShouldNotBeNull
+        subject.ItemType.ShouldNotBeNull
+        subject.ItemTypeCharacterStatisticBuff.ShouldNotBeNull
+        subject.ItemTypeEquipSlot.ShouldNotBeNull
+        subject.ItemTypeEvent.ShouldNotBeNull
+        subject.ItemTypeShopType.ShouldNotBeNull
+        subject.ItemTypeSpawnCount.ShouldNotBeNull
+        subject.ItemTypeSpawnLocationType.ShouldNotBeNull
+        subject.ItemTypeStatistic.ShouldNotBeNull
+        subject.ItemTypeStatisticType.ShouldNotBeNull
+        subject.Location.ShouldNotBeNull
+        subject.LocationDungeonLevel.ShouldNotBeNull
+        subject.LocationStatistic.ShouldNotBeNull
+        subject.LocationType.ShouldNotBeNull
+        subject.Player.ShouldNotBeNull
+        subject.QuestType.ShouldNotBeNull
+        subject.Route.ShouldNotBeNull
+        subject.RouteType.ShouldNotBeNull
+        subject.RouteTypeLock.ShouldNotBeNull
+        subject.ShoppeType.ShouldNotBeNull
+        subject.SpellType.ShouldNotBeNull
+        subject.SpellTypeRequiredPower.ShouldNotBeNull
+        subject.Events.ShouldNotBeNull
+        store.VerifyNoOtherCalls()
+        checker.VerifyNoOtherCalls()
+    End Sub
+    <Fact>
+    Sub ShouldSaveToTheStore()
+        Dim store As New Mock(Of IStore)
+        Dim checker As New Mock(Of IEventData)
+        Dim subject As IWorldData = New WorldData(store.Object, checker.Object)
+        Const filename = "filename"
+        subject.Save(filename)
+        store.Verify(Sub(x) x.Save(filename), Times.Once)
+        store.VerifyNoOtherCalls()
+        checker.VerifyNoOtherCalls()
+    End Sub
+    <Fact>
+    Sub ShouldLoadFromTheStore()
+        Dim store As New Mock(Of IStore)
+        Dim checker As New Mock(Of IEventData)
+        Dim subject As IWorldData = New WorldData(store.Object, checker.Object)
+        Const filename = "filename"
+        subject.Load(filename)
+        store.Verify(Sub(x) x.Load(filename), Times.Once)
+        store.VerifyNoOtherCalls()
+        checker.VerifyNoOtherCalls()
+    End Sub
+    <Fact>
+    Sub ShouldResetTheStore()
+        Dim store As New Mock(Of IStore)
+        Dim checker As New Mock(Of IEventData)
+        Dim subject As IWorldData = New WorldData(store.Object, checker.Object)
+        subject.Reset()
+        store.Verify(Sub(x) x.Reset(), Times.Once)
+        store.VerifyNoOtherCalls()
+        checker.VerifyNoOtherCalls()
+    End Sub
+    <Fact>
+    Sub ShouldRenewTheStore()
+        Dim store As New Mock(Of IStore)
+        Dim checker As New Mock(Of IEventData)
+        Dim subject As IWorldData = New WorldData(store.Object, checker.Object)
+        subject.Renew().ShouldBeNull
+        store.Verify(Function(x) x.Renew(), Times.Once)
+        store.VerifyNoOtherCalls()
+        checker.VerifyNoOtherCalls()
+    End Sub
+    <Fact>
+    Sub ShouldRestoreTheStore()
+        Dim store As New Mock(Of IStore)
+        Dim checker As New Mock(Of IEventData)
+        Dim subject As IWorldData = New WorldData(store.Object, checker.Object)
+        subject.Restore(Nothing)
+        store.Verify(Sub(x) x.Restore(Nothing), Times.Once)
+        store.VerifyNoOtherCalls()
+        checker.VerifyNoOtherCalls()
+    End Sub
+End Class
+
 
--- src/KordanorsCabal.Game.Tests/RouteShould.vb
+++ src/KordanorsCabal.Game.Tests/RouteShould.vb
@@ -4,4 +4,61 @@
     Public Sub New()
         MyBase.New(AddressOf Route.FromId)
     End Sub
+    <Fact>
+    Sub have_route_type()
+        WithSubject(
+            Sub(worldData, id, subject)
+                Const routeTypeId = 2L
+                worldData.Setup(Function(x) x.Route.ReadRouteType(It.IsAny(Of Long))).Returns(routeTypeId)
+                subject.RouteType.ShouldBe(OldRouteType.Passageway)
+                worldData.Verify(Function(x) x.Route.ReadRouteType(id))
+            End Sub)
+    End Sub
+    <Fact>
+    Sub destroy()
+        WithSubject(
+            Sub(worldData, id, subject)
+                worldData.Setup(Sub(x) x.Route.Clear(It.IsAny(Of Long)))
+                subject.Destroy()
+                worldData.Verify(Sub(x) x.Route.Clear(id))
+            End Sub)
+    End Sub
+    <Fact>
+    Sub have_can_move()
+        WithSubject(
+            Sub(worldData, id, subject)
+                Const characterId = 2L
+                Const routeTypeId = 3L
+                worldData.Setup(Function(x) x.Route.ReadRouteType(It.IsAny(Of Long))).Returns(routeTypeId)
+                worldData.Setup(Function(x) x.RouteTypeLock.ReadUnlockItem(It.IsAny(Of Long)))
+                subject.CanMove(Character.FromId(worldData.Object, characterId))
+                worldData.Verify(Function(x) x.Route.ReadRouteType(id))
+                worldData.Verify(Function(x) x.RouteTypeLock.ReadUnlockItem(routeTypeId))
+            End Sub)
+    End Sub
+    <Fact>
+    Sub have_to_location()
+        WithSubject(
+            Sub(worldData, id, subject)
+                worldData.Setup(Function(x) x.Route.ReadToLocation(It.IsAny(Of Long)))
+                subject.ToLocation.ShouldBeNull
+                worldData.Verify(Function(x) x.Route.ReadToLocation(id))
+            End Sub)
+    End Sub
+    <Fact>
+    Sub move()
+        WithSubject(
+            Sub(worldData, id, subject)
+                Const characterId = 2L
+                Const routeTypeId = 3L
+                worldData.Setup(Function(x) x.Route.ReadRouteType(It.IsAny(Of Long))).Returns(routeTypeId)
+                worldData.Setup(Function(x) x.RouteTypeLock.ReadUnlockItem(It.IsAny(Of Long)))
+                worldData.Setup(Function(x) x.RouteType.ReadIsSingleUse(It.IsAny(Of Long)))
+                subject.Move(Character.FromId(worldData.Object, characterId))
+                worldData.Verify(Function(x) x.Route.ReadRouteType(id))
+                worldData.Verify(Function(x) x.Route.ReadToLocation(id))
+                worldData.Verify(Function(x) x.RouteTypeLock.ReadUnlockItem(routeTypeId))
+                worldData.Verify(Function(x) x.RouteType.ReadIsSingleUse(routeTypeId))
+            End Sub)
+    End Sub
 End Class
