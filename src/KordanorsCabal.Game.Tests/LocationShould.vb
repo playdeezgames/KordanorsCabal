3a09a24cb8221f6547267c4b1a45c10b63e0776b
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-10-11 05:28:18 -0500

    have_can_map

--- src/KordanorsCabal.Game.Tests/LocationShould.vb
+++ src/KordanorsCabal.Game.Tests/LocationShould.vb
@@ -57,14 +57,24 @@
     Sub have_requires_mp()
         WithSubject(
             Sub(worldData, id, subject)
+                Const locationTypeId = 2L
+                worldData.Setup(Function(x) x.Location.ReadLocationType(It.IsAny(Of Long))).Returns(locationTypeId)
+                worldData.Setup(Function(x) x.LocationType.ReadRequiresMP(It.IsAny(Of Long)))
                 subject.RequiresMP.ShouldBeFalse
+                worldData.Verify(Function(x) x.Location.ReadLocationType(id))
+                worldData.Verify(Function(x) x.LocationType.ReadRequiresMP(locationTypeId))
             End Sub)
     End Sub
     <Fact>
     Sub have_can_map()
         WithSubject(
             Sub(worldData, id, subject)
+                Const locationTypeId = 2L
+                worldData.Setup(Function(x) x.Location.ReadLocationType(It.IsAny(Of Long))).Returns(locationTypeId)
+                worldData.Setup(Function(x) x.LocationType.ReadCanMap(It.IsAny(Of Long)))
                 subject.CanMap.ShouldBeFalse
+                worldData.Verify(Function(x) x.Location.ReadLocationType(id))
+                worldData.Verify(Function(x) x.LocationType.ReadCanMap(locationTypeId))
             End Sub)
     End Sub
     <Fact>
@@ -78,7 +88,12 @@
     Sub have_is_dungeon()
         WithSubject(
             Sub(worldData, id, subject)
+                Const locationTypeId = 2L
+                worldData.Setup(Function(x) x.Location.ReadLocationType(It.IsAny(Of Long))).Returns(locationTypeId)
+                worldData.Setup(Function(x) x.LocationType.ReadIsDungeon(It.IsAny(Of Long)))
                 subject.IsDungeon.ShouldBeFalse
+                worldData.Verify(Function(x) x.Location.ReadLocationType(id))
+                worldData.Verify(Function(x) x.LocationType.ReadIsDungeon(locationTypeId))
             End Sub)
     End Sub
     <Fact>
