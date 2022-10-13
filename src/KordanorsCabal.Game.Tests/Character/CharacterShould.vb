359e15fccf233224ab4e9bb684e1e3b6da139ecf
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-10-13 05:49:42 -0500

    have_is_encumbered

--- src/KordanorsCabal.Game.Tests/Character/CharacterShould.vb
+++ src/KordanorsCabal.Game.Tests/Character/CharacterShould.vb
@@ -514,7 +514,22 @@ Public Class CharacterShould
     Sub have_is_encumbered()
         WithSubject(
             Sub(worldData, id, subject)
+                worldData.Setup(Function(x) x.Inventory.ReadForCharacter(It.IsAny(Of Long)))
+                worldData.Setup(Function(x) x.InventoryItem.ReadItems(It.IsAny(Of Long)))
+                worldData.Setup(Function(x) x.CharacterEquipSlot.ReadItemsForCharacter(It.IsAny(Of Long)))
+                worldData.Setup(Function(x) x.CharacterStatistic.Read(It.IsAny(Of Long), It.IsAny(Of Long)))
+                worldData.Setup(Function(x) x.CharacterStatisticType.ReadDefaultValue(It.IsAny(Of Long)))
                 subject.IsEncumbered.ShouldBeFalse
+                worldData.Verify(Function(x) x.Inventory.ReadForCharacter(id))
+                worldData.Verify(Function(x) x.Inventory.CreateForCharacter(id))
+                worldData.Verify(Function(x) x.InventoryItem.ReadItems(0))
+                worldData.Verify(Function(x) x.CharacterEquipSlot.ReadItemsForCharacter(id))
+                worldData.Verify(Function(x) x.CharacterStatistic.Read(id, 1))
+                worldData.Verify(Function(x) x.CharacterStatistic.Read(id, 24))
+                worldData.Verify(Function(x) x.CharacterStatistic.Read(id, 25))
+                worldData.Verify(Function(x) x.CharacterStatisticType.ReadDefaultValue(1))
+                worldData.Verify(Function(x) x.CharacterStatisticType.ReadDefaultValue(24))
+                worldData.Verify(Function(x) x.CharacterStatisticType.ReadDefaultValue(25))
             End Sub)
     End Sub
     <Fact>
