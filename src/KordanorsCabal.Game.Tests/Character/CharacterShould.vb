d6c9e7c8ef5f1a60eabc7daf3495c0fb1a34a31d
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-10-13 05:00:07 -0500

    have_chafing

--- src/KordanorsCabal.Game.Tests/Character/CharacterShould.vb
+++ src/KordanorsCabal.Game.Tests/Character/CharacterShould.vb
@@ -416,7 +416,13 @@ Public Class CharacterShould
     Sub have_chafing()
         WithSubject(
             Sub(worldData, id, subject)
+                worldData.Setup(Function(x) x.CharacterStatistic.Read(It.IsAny(Of Long), It.IsAny(Of Long)))
+                worldData.Setup(Function(x) x.CharacterStatisticType.ReadDefaultValue(It.IsAny(Of Long))).Returns(0)
+                worldData.Setup(Function(x) x.CharacterEquipSlot.ReadItemsForCharacter(It.IsAny(Of Long)))
                 subject.Chafing.ShouldBe(0)
+                worldData.Verify(Function(x) x.CharacterStatistic.Read(id, 22))
+                worldData.Verify(Function(x) x.CharacterStatisticType.ReadDefaultValue(22))
+                worldData.Verify(Function(x) x.CharacterEquipSlot.ReadItemsForCharacter(id))
             End Sub)
     End Sub
     <Fact>
