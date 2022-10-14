aa84729f6b77944115cb59c87834b28fe9052de2
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-10-14 04:43:41 -0500

    set statistic

--- src/KordanorsCabal.Game.Tests/Character/CharacterShould.vb
+++ src/KordanorsCabal.Game.Tests/Character/CharacterShould.vb
@@ -863,7 +863,13 @@ Public Class CharacterShould
             Sub(worldData, id, subject)
                 Const statisticTypeId = 2L
                 Const statisticValue = 3L
+                worldData.Setup(Function(x) x.CharacterStatisticType.ReadMinimumValue(It.IsAny(Of Long))).Returns(0)
+                worldData.Setup(Function(x) x.CharacterStatisticType.ReadMaximumValue(It.IsAny(Of Long))).Returns(10)
+                worldData.Setup(Sub(x) x.CharacterStatistic.Write(It.IsAny(Of Long), It.IsAny(Of Long), It.IsAny(Of Long)))
                 subject.SetStatistic(CharacterStatisticType.FromId(worldData.Object, statisticTypeId), statisticValue)
+                worldData.Verify(Function(x) x.CharacterStatisticType.ReadMinimumValue(statisticTypeId))
+                worldData.Verify(Function(x) x.CharacterStatisticType.ReadMaximumValue(statisticTypeId))
+                worldData.Verify(Sub(x) x.CharacterStatistic.Write(id, statisticTypeId, statisticValue))
             End Sub)
     End Sub
     <Fact>
