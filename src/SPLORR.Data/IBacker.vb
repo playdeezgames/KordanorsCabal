2254458d6e5733cbe69416ef5cf40c8ef1472a2e
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-10-14 10:47:23 -0500

    backer and ibacker in their own files

---
+++ src/SPLORR.Data/IBacker.vb
@@ -0,0 +1,13 @@
+﻿Imports Microsoft.Data.Sqlite
+
+Public Interface IBacker
+    Property Connection As SqliteConnection
+    ReadOnly Property LastInsertRowId() As Long
+    Sub BackupTo(other As IBacker)
+    Sub Connect(filename As String)
+    Sub ShutDown()
+    Sub ExecuteNonQuery(sql As String, ParamArray parameters() As (String, Object))
+    Function ExecuteScalar(Of TResult As Structure)(query As String, ParamArray parameters() As (String, Object)) As TResult?
+    Function ExecuteScalar(Of TResult As Class)(transform As Func(Of Object, TResult), query As String, ParamArray parameters() As (String, Object)) As TResult
+    Function ExecuteReader(Of TResult)(transform As Func(Of Func(Of String, Object), TResult), query As String, ParamArray parameters() As (String, Object)) As List(Of TResult)
+End Interface
