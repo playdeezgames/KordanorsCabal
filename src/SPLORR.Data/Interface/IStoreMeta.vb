32796ee290ba834b22c4b7e91c2b28e356ce1524
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-10-14 17:06:36 -0500

    meta needs no rename

--- src/SPLORR.Data/Interface/IStoreMeta.vb
+++ src/SPLORR.Data/Interface/IStoreMeta.vb
@@ -1,12 +1,8 @@
 ﻿Public Interface IStoreMeta
-
     Sub Reset()
     Function Renew() As IBacker
-    Sub Restore(
-               oldBacker As IBacker)
+    Sub Restore(oldBacker As IBacker)
     Sub ShutDown()
-    Sub Save(
-            filename As String)
-    Sub Load(
-            filename As String)
+    Sub Save(filename As String)
+    Sub Load(filename As String)
 End Interface
