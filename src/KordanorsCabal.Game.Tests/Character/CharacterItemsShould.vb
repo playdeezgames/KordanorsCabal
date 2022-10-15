717417d2d7a2ffada5642ce462b65c80187a39f2
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-10-15 16:57:03 -0500

    CharacterItemsShould

---
+++ src/KordanorsCabal.Game.Tests/Character/CharacterItemsShould.vb
@@ -0,0 +1,6 @@
+﻿Public Class CharacterItemsShould
+    Inherits ThingieShould(Of ICharacterItems)
+    Sub New()
+        MyBase.New(Function(w, i) CharacterItems.FromCharacter(w, Character.FromId(w, i)))
+    End Sub
+End Class
