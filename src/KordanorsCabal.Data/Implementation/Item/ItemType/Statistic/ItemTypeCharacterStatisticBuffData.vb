2de48335389fda77ff4b1b97cb8ee4dc08accd4c
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-10-16 17:16:49 -0500

    ItemTypeCharacterStatisticBuffs

--- src/KordanorsCabal.Data/Implementation/Item/ItemType/Statistic/ItemTypeCharacterStatisticBuffData.vb
+++ src/KordanorsCabal.Data/Implementation/Item/ItemType/Statistic/ItemTypeCharacterStatisticBuffData.vb
@@ -5,34 +5,13 @@
     Friend Const ItemTypeIdColumn = ItemTypeData.ItemTypeIdColumn
     Friend Const CharacterStatisticTypeIdColumn = CharacterStatisticTypeData.CharacterStatisticTypeIdColumn
     Friend Const BuffColumn = "Buff"
-    Friend Sub Initialize()
-        Store.Primitive.Execute($"CREATE TABLE IF NOT EXISTS [{TableName}] AS
-                WITH [cte](
-                    [{ItemTypeIdColumn}],
-                    [{CharacterStatisticTypeIdColumn}],
-                    [{BuffColumn}]) AS
-                (VALUES
-                    (45,6,1),
-                    (46,6,1),
-                    (49,2,1),
-                    (50,5,1),
-                    (51,8,1)
-                )
-                SELECT 
-                    [{ItemTypeIdColumn}],
-                    [{CharacterStatisticTypeIdColumn}],
-                    [{BuffColumn}]
-                FROM [cte];")
-    End Sub
-
-
     Public Sub New(store As IStore, world As IWorldData)
         MyBase.New(store, world)
     End Sub
 
     Public Function Read(itemTypeId As Long, characterStatisticTypeId As Long) As Long? Implements IItemTypeCharacterStatisticBuffData.Read
         Return Store.Column.ReadValue(Of Long, Long, Long)(
-            AddressOf Initialize,
+            AddressOf NoInitializer,
             TableName,
             BuffColumn,
             (ItemTypeIdColumn, itemTypeId),
