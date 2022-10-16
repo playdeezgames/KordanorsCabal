13634710be9c2d430e0675ed30d5ea12a73993f7
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-10-16 17:08:59 -0500

    ItemTypeSpawnCounts

--- src/KordanorsCabal.Data/Implementation/Item/ItemType/Spawn/ItemTypeSpawnCountData.vb
+++ src/KordanorsCabal.Data/Implementation/Item/ItemType/Spawn/ItemTypeSpawnCountData.vb
@@ -4,55 +4,16 @@
     Friend Const TableName = "ItemTypeSpawnCounts"
     Friend Const ItemTypeIdColumn = ItemTypeData.ItemTypeIdColumn
     Friend Const DungeonLevelIdColumn = DungeonLevelData.DungeonLevelIdColumn
-    Friend Const SpawnDice = "SpawnDice"
-    Friend Sub Initialize()
-        Store.Primitive.Execute($"CREATE TABLE IF NOT EXISTS [{TableName}] AS
-                WITH [cte](
-                    [{ItemTypeIdColumn}],
-                    [{DungeonLevelIdColumn}],
-                    [{SpawnDice}]) AS
-                (VALUES
-                    (7,1,'3d6'),
-                    (7,2,'3d6'),
-                    (7,3,'3d6'),
-                    (7,4,'3d6'),
-                    (7,5,'3d6'),
-                    (10,1,'4d6'),
-                    (10,2,'2d6'),
-                    (11,2,'1d1'),
-                    (12,4,'1d1'),
-                    (13,3,'1d1'),
-                    (14,1,'1d1'),
-                    (15,1,'3d6'),
-                    (16,1,'3d6'),
-                    (17,1,'1d6'),
-                    (18,1,'2d6'),
-                    (18,2,'1d6'),
-                    (19,2,'2d6'),
-                    (19,3,'1d6'),
-                    (20,2,'1d6'),
-                    (43,6,'1d1'),
-                    (45,1,'1d1'),
-                    (48,4,'1d1'),
-                    (50,3,'1d1'),
-                    (51,8,'1d1')
-                )
-                SELECT 
-                    [{ItemTypeIdColumn}],
-                    [{DungeonLevelIdColumn}],
-                    [{SpawnDice}]
-                FROM [cte];")
-    End Sub
-
+    Friend Const SpawnDiceColumn = "SpawnDice"
     Public Sub New(store As IStore, world As IWorldData)
         MyBase.New(store, world)
     End Sub
 
     Public Function Read(itemTypeId As Long, dungeonLevelId As Long) As String Implements IItemTypeSpawnCountData.Read
         Return Store.Column.ReadString(
-            AddressOf Initialize,
+            AddressOf NoInitializer,
             TableName,
-            SpawnDice,
+            SpawnDiceColumn,
             (ItemTypeIdColumn, itemTypeId),
             (DungeonLevelIdColumn, dungeonLevelId))
     End Function
