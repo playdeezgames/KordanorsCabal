20fed478909e10cf75c86a603f5c7252b144946d
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-10-16 16:53:38 -0500

    ItemTypeEquipSlots

--- src/KordanorsCabal.Data/Implementation/Item/ItemType/ItemTypeEquipSlotData.vb
+++ src/KordanorsCabal.Data/Implementation/Item/ItemType/ItemTypeEquipSlotData.vb
@@ -8,40 +8,9 @@
     Public Sub New(store As IStore, world As IWorldData)
         MyBase.New(store, world)
     End Sub
-    Friend Sub Initialize()
-        Store.Primitive.Execute($"CREATE TABLE IF NOT EXISTS [{TableName}] AS
-                WITH [cte](
-                    [{ItemTypeIdColumn}],
-                    [{EquipSlotIdColumn}]) AS
-                (VALUES
-                    (10,1),
-                    (15,2),
-                    (16,3),
-                    (17,4),
-                    (18,1),
-                    (19,1),
-                    (20,4),
-                    (27,5),
-                    (30,1),
-                    (43,1),
-                    (45,6),
-                    (46,7),
-                    (46,8),
-                    (48,6),
-                    (49,6),
-                    (50,6),
-                    (51,6),
-                    (52,6)
-                )
-                SELECT 
-                    [{ItemTypeIdColumn}],
-                    [{EquipSlotIdColumn}]
-                FROM [cte];")
-    End Sub
-
     Public Function ReadForItemType(itemTypeId As Long) As IEnumerable(Of Long) Implements IItemTypeEquipSlotData.ReadForItemType
         Return Store.Record.WithValues(Of Long, Long)(
-            AddressOf Initialize,
+            AddressOf NoInitializer,
             TableName,
             EquipSlotIdColumn,
             (ItemTypeIdColumn, itemTypeId))
