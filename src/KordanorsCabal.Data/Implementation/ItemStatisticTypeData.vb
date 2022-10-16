8db487998e80ad9aefa021bac0ccb7bd3d9f5cb9
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-10-16 16:50:09 -0500

    ItemStatisticTypes

--- src/KordanorsCabal.Data/Implementation/ItemStatisticTypeData.vb
+++ src/KordanorsCabal.Data/Implementation/ItemStatisticTypeData.vb
@@ -4,26 +4,13 @@
     Friend Const TableName = "ItemStatisticTypes"
     Friend Const ItemStatisticTypeIdColumn = "ItemStatisticTypeId"
     Friend Const DefaultValueColumn = "DefaultValue"
-    Friend Sub Initialize()
-        Store.Primitive.Execute($"CREATE TABLE IF NOT EXISTS [{TableName}] AS
-                WITH [cte](
-                    [{ItemStatisticTypeIdColumn}],
-                    [{DefaultValueColumn}]) AS
-                (VALUES
-                    (1,0))
-                SELECT 
-                    [{ItemStatisticTypeIdColumn}],
-                    [{DefaultValueColumn}]
-                FROM [cte];")
-    End Sub
-
     Public Sub New(store As IStore, world As IWorldData)
         MyBase.New(store, world)
     End Sub
 
     Public Function ReadDefaultValue(statisticTypeId As Long) As Long? Implements IItemStatisticTypeData.ReadDefaultValue
         Return Store.Column.ReadValue(Of Long, Long)(
-            AddressOf Initialize,
+            AddressOf NoInitializer,
             TableName,
             DefaultValueColumn,
             (ItemStatisticTypeIdColumn, statisticTypeId))
