f7fd58ff98560b122ae2f7dd08f0a0f78ef2e873
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-10-16 16:59:38 -0500

    ShoppeTypes

--- src/KordanorsCabal.Data/Implementation/Location/ShoppeTypeData.vb
+++ src/KordanorsCabal.Data/Implementation/Location/ShoppeTypeData.vb
@@ -4,31 +4,13 @@
     Friend Const TableName = "ShoppeTypes"
     Friend Const ShoppeTypeIdColumn = "ShoppeTypeId"
     Friend Const ShoppeTypeNameColumn = "ShoppeTypeName"
-
-    Friend Sub Initialize()
-        Store.Primitive.Execute($"CREATE TABLE IF NOT EXISTS [{TableName}] AS
-                WITH [cte](
-                    [{ShoppeTypeIdColumn}],
-                    [{ShoppeTypeNameColumn}]) AS
-                (VALUES
-                    (1,'Magic'),
-                    (2,'Blacksmith'),
-                    (3,'Innkeeper'),
-                    (4,'Healer'),
-                    (5,'Black Market'))
-                SELECT 
-                    [{ShoppeTypeIdColumn}],
-                    [{ShoppeTypeNameColumn}]
-                FROM [cte];")
-    End Sub
-
     Public Sub New(store As IStore, world As IWorldData)
         MyBase.New(store, world)
     End Sub
 
     Public Function ReadName(shoppeTypeId As Long) As String Implements IShoppeTypeData.ReadName
         Return Store.Column.ReadString(
-            AddressOf Initialize,
+            AddressOf NoInitializer,
             TableName,
             ShoppeTypeNameColumn,
             (ShoppeTypeIdColumn, shoppeTypeId))
