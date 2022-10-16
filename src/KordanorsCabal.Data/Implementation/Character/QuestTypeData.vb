6816d0af476e120ddbae1301541013955817b460
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-10-16 16:46:59 -0500

    QuestTypes

--- src/KordanorsCabal.Data/Implementation/Character/QuestTypeData.vb
+++ src/KordanorsCabal.Data/Implementation/Character/QuestTypeData.vb
@@ -7,47 +7,27 @@
     Friend Const AcceptEventNameColumn = "AcceptEventName"
     Friend Const CanCompleteEventNameColumn = "CanCompleteEventName"
     Friend Const CompleteEventNameColumn = "CompleteEventName"
-
-    Friend Sub Initialize()
-        Store.Primitive.Execute($"CREATE TABLE IF NOT EXISTS [{TableName}] AS
-                WITH [cte](
-                    [{QuestTypeIdColumn}],
-                    [{CanAcceptEventNameColumn}],
-                    [{AcceptEventNameColumn}],
-                    [{CanCompleteEventNameColumn}],
-                    [{CompleteEventNameColumn}]) AS
-                (VALUES
-                    (1,'CharacterCanAcceptCellarRatsQuest','CharacterAcceptCellarRatsQuest','CharacterCanCompleteCellarRatsQuest','CharacterCompleteCellarRatsQuest'))
-                SELECT 
-                    [{QuestTypeIdColumn}],
-                    [{CanAcceptEventNameColumn}],
-                    [{AcceptEventNameColumn}],
-                    [{CanCompleteEventNameColumn}],
-                    [{CompleteEventNameColumn}]
-                FROM [cte];")
-    End Sub
-
     Public Sub New(store As IStore, world As IWorldData)
         MyBase.New(store, world)
     End Sub
 
     Public Function ReadCanAcceptEventName(questTypeId As Long) As String Implements IQuestTypeData.ReadCanAcceptEventName
-        Return Store.Column.ReadString(AddressOf Initialize, TableName, CanAcceptEventNameColumn, (QuestTypeIdColumn, questTypeId))
+        Return Store.Column.ReadString(AddressOf NoInitializer, TableName, CanAcceptEventNameColumn, (QuestTypeIdColumn, questTypeId))
     End Function
 
     Public Function ReadCanCompleteEventName(questTypeId As Long) As String Implements IQuestTypeData.ReadCanCompleteEventName
-        Return Store.Column.ReadString(AddressOf Initialize, TableName, CanCompleteEventNameColumn, (QuestTypeIdColumn, questTypeId))
+        Return Store.Column.ReadString(AddressOf NoInitializer, TableName, CanCompleteEventNameColumn, (QuestTypeIdColumn, questTypeId))
     End Function
 
     Public Function ReadAcceptEventName(questTypeId As Long) As String Implements IQuestTypeData.ReadAcceptEventName
-        Return Store.Column.ReadString(AddressOf Initialize, TableName, AcceptEventNameColumn, (QuestTypeIdColumn, questTypeId))
+        Return Store.Column.ReadString(AddressOf NoInitializer, TableName, AcceptEventNameColumn, (QuestTypeIdColumn, questTypeId))
     End Function
 
     Public Function ReadCompleteEventName(questTypeId As Long) As String Implements IQuestTypeData.ReadCompleteEventName
-        Return Store.Column.ReadString(AddressOf Initialize, TableName, CompleteEventNameColumn, (QuestTypeIdColumn, questTypeId))
+        Return Store.Column.ReadString(AddressOf NoInitializer, TableName, CompleteEventNameColumn, (QuestTypeIdColumn, questTypeId))
     End Function
 
     Public Function ReadAll() As IEnumerable(Of Long) Implements IQuestTypeData.ReadAll
-        Return Store.Record.All(Of Long)(AddressOf Initialize, TableName, QuestTypeIdColumn)
+        Return Store.Record.All(Of Long)(AddressOf NoInitializer, TableName, QuestTypeIdColumn)
     End Function
 End Class
