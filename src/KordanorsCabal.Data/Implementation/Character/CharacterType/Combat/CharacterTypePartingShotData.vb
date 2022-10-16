bdc8ec6ed4bc3e6d815e4fe2cd3b431da4f0aedb
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-10-16 15:55:59 -0500

    CharacterTypePartingShots

--- src/KordanorsCabal.Data/Implementation/Character/CharacterType/Combat/CharacterTypePartingShotData.vb
+++ src/KordanorsCabal.Data/Implementation/Character/CharacterType/Combat/CharacterTypePartingShotData.vb
@@ -5,32 +5,13 @@
     Friend Const CharacterTypeIdColumn = CharacterTypeData.CharacterTypeIdColumn
     Friend Const PartingShotColumn = "PartingShot"
     Friend Const WeightColumn = "Weight"
-    Friend Sub Initialize()
-        Store.Primitive.Execute($"CREATE TABLE IF NOT EXISTS [{TableName}] AS
-                WITH [cte](
-                    [{CharacterTypeIdColumn}],
-                    [{PartingShotColumn}],
-                    [{WeightColumn}]) AS
-                (VALUES
-                    (6,'@#$% you!',1),
-                    (7,'@#$% you!',1),
-                    (16,'Breains!',1)
-                )
-                SELECT 
-                    [{CharacterTypeIdColumn}],
-                    [{PartingShotColumn}],
-                    [{WeightColumn}]
-                FROM [cte];")
-
-    End Sub
-
     Public Sub New(store As IStore, world As IWorldData)
         MyBase.New(store, world)
     End Sub
 
     Public Function Read(characterTypeId As Long) As IReadOnlyDictionary(Of String, Integer) Implements ICharacterTypePartingShotData.Read
         Dim results = Store.Record.WithValue(Of Long, String, Long)(
-            AddressOf Initialize,
+            AddressOf NoInitializer,
             TableName,
             (PartingShotColumn, WeightColumn),
             (CharacterTypeIdColumn, characterTypeId))
