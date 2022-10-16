6b289b1d9c62320786ed399bedf8e729a06badde
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-10-16 17:19:45 -0500

    ItemTypeStatisticTypes

--- src/KordanorsCabal.Data/Implementation/Item/ItemType/Statistic/ItemTypeStatisticTypeData.vb
+++ src/KordanorsCabal.Data/Implementation/Item/ItemType/Statistic/ItemTypeStatisticTypeData.vb
@@ -7,30 +7,11 @@
 
     Public Function ReadName(itemTypeStatisticTypeId As Long) As String Implements IItemTypeStatisticTypeData.ReadName
         Return Store.Column.ReadString(
-            AddressOf Initialize,
+            AddressOf NoInitializer,
             TableName,
             ItemTypeStatisticTypeNameColumn,
             (ItemTypeStatisticTypeIdColumn, itemTypeStatisticTypeId))
     End Function
-    Friend Sub Initialize()
-        Store.Primitive.Execute($"CREATE TABLE IF NOT EXISTS [ItemTypeStatisticTypes] AS
-                WITH [cte](
-                    [ItemTypeStatisticTypeId],
-                    [ItemTypeStatisticTypeName]) AS
-                (VALUES
-                    (1,'Encumbrance'),
-                    (2,'AttackDice'),
-                    (3,'MaximumDamage'),
-                    (4,'DefendDice'),
-                    (5,'MaximumDurability'),
-                    (6,'Offer'),
-                    (7,'Price'),
-                    (8,'RepairPrice'))
-                SELECT 
-                    [{ItemTypeStatisticTypeIdColumn}],
-                    [{ItemTypeStatisticTypeNameColumn}]
-                FROM [cte];")
-    End Sub
     Public Sub New(store As IStore, world As IWorldData)
         MyBase.New(store, world)
     End Sub
