46b9251fdd9dd7bd5d9cec4da1c3eede33ed7694
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-10-16 19:30:43 -0500

    SpellTypeRequiredPowers

--- src/KordanorsCabal.Data/Implementation/Spell/SpellTypeRequiredPowerData.vb
+++ src/KordanorsCabal.Data/Implementation/Spell/SpellTypeRequiredPowerData.vb
@@ -5,30 +5,13 @@
     Friend Const SpellTypeIdColumn = SpellTypeData.SpellTypeIdColumn
     Friend Const SpellLevelColumn = "SpellLevel"
     Friend Const PowerColumn = "Power"
-    Friend Sub Initialize()
-        Store.Primitive.Execute($"CREATE TABLE IF NOT EXISTS [{TableName}] AS
-                WITH [cte](
-                    [{SpellTypeIdColumn}],
-                    [{SpellLevelColumn}],
-                    [{PowerColumn}]) AS
-                (VALUES
-                    (1,0,0),
-                    (1,1,1),
-                    (2,0,0),
-                    (2,1,0))
-                SELECT 
-                    [{SpellTypeIdColumn}],
-                    [{SpellLevelColumn}],
-                    [{PowerColumn}]
-                FROM [cte];")
-    End Sub
     Public Sub New(store As IStore, world As IWorldData)
         MyBase.New(store, world)
     End Sub
 
     Public Function Read(spellTypeId As Long, level As Long) As Long? Implements ISpellTypeRequiredPowerData.Read
         Return Store.Column.ReadValue(Of Long, Long, Long)(
-            AddressOf Initialize,
+            AddressOf NoInitializer,
             TableName,
             PowerColumn,
             (SpellTypeIdColumn, spellTypeId),
