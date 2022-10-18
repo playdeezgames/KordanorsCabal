a36c2d0fd6adbded0ca3a8fd3043f332b1e82d22
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-10-18 15:08:52 -0500

    new boilerplate. keeping OLD boilerplate a while, tho.

--- src/KordanorsCabal.Scaffolder/Program.vb
+++ src/KordanorsCabal.Scaffolder/Program.vb
@@ -2,7 +2,7 @@ Module Program
     Sub Main(args As String())
         Dim start = DateTimeOffset.Now
         System.IO.File.Delete("test.db")
-        Using connection As New SqliteConnection("Data Source=:memory:")
+        Using connection As New SqliteConnection("Data Source=test.db")
             connection.Open()
             Scaffold.Scaffold(connection)
             Populate.Populate(connection)
