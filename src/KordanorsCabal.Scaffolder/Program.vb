65cae008015b1d91591611a9efe5cb29401e56f4
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-10-18 15:57:09 -0500

    automagical update of the boilerplate

--- src/KordanorsCabal.Scaffolder/Program.vb
+++ src/KordanorsCabal.Scaffolder/Program.vb
@@ -1,14 +1,11 @@
 Module Program
     Sub Main(args As String())
-        Dim start = DateTimeOffset.Now
-        IO.File.Delete("test.db")
-        Using connection As New SqliteConnection("Data Source=test.db")
+        Dim filename = args(0)
+        IO.File.Delete(filename)
+        Using connection As New SqliteConnection($"Data Source={filename}")
             connection.Open()
             Scaffold(connection)
             Populate(connection)
         End Using
-        Dim elapsed = DateTimeOffset.Now - start
-        Console.WriteLine(elapsed)
-        Console.ReadLine()
     End Sub
 End Module
