3f6862587710b35689857ee2ae8ed74367b00239
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-10-18 05:49:03 -0500

    timing and to memory

--- src/KordanorsCabal.Scaffolder/Program.vb
+++ src/KordanorsCabal.Scaffolder/Program.vb
@@ -1,10 +1,14 @@
 Module Program
     Sub Main(args As String())
+        Dim start = DateTimeOffset.Now
         System.IO.File.Delete("test.db")
-        Using connection As New SqliteConnection("Data Source=test.db")
+        Using connection As New SqliteConnection("Data Source=:memory:")
             connection.Open()
             Scaffold.Scaffold(connection)
             Populate.Populate(connection)
         End Using
+        Dim elapsed = DateTimeOffset.Now - start
+        Console.WriteLine(elapsed)
+        Console.ReadLine()
     End Sub
 End Module
