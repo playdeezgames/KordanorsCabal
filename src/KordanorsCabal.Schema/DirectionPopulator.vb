05109eadbf63af287b9bc5eab331c86a36e8c13c
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-10-21 04:27:27 -0500

    feature types and directions

---
+++ src/KordanorsCabal.Schema/DirectionPopulator.vb
@@ -0,0 +1,32 @@
+﻿Module DirectionPopulator
+    Private Sub PopulateDirectionsRecord(connection As SqliteConnection, DirectionId As Long, DirectionName As String, Abbreviation As String, IsCardinal As Long, PreviousDirectionId As Long?, OppositeDirectionId As Long, NextDirectionId As Long?)
+        Using command = New SqliteCommand($"INSERT INTO [{Tables.Directions}]([{Columns.DirectionIdColumn}], [{Columns.DirectionNameColumn}], [{Columns.AbbreviationColumn}], [{Columns.IsCardinalColumn}], [{Columns.PreviousDirectionIdColumn}], [{Columns.OppositeDirectionIdColumn}], [{Columns.NextDirectionIdColumn}]) VALUES (@{Columns.DirectionIdColumn}, @{Columns.DirectionNameColumn}, @{Columns.AbbreviationColumn}, @{Columns.IsCardinalColumn}, @{Columns.PreviousDirectionIdColumn}, @{Columns.OppositeDirectionIdColumn}, @{Columns.NextDirectionIdColumn});", connection)
+            command.Parameters.AddWithValue($"@{Columns.DirectionIdColumn}", DirectionId)
+            command.Parameters.AddWithValue($"@{Columns.DirectionNameColumn}", DirectionName)
+            command.Parameters.AddWithValue($"@{Columns.AbbreviationColumn}", Abbreviation)
+            command.Parameters.AddWithValue($"@{Columns.IsCardinalColumn}", IsCardinal)
+            command.Parameters.AddWithValue($"@{Columns.PreviousDirectionIdColumn}", If(PreviousDirectionId Is Nothing, CObj(DBNull.Value), PreviousDirectionId))
+            command.Parameters.AddWithValue($"@{Columns.OppositeDirectionIdColumn}", OppositeDirectionId)
+            command.Parameters.AddWithValue($"@{Columns.NextDirectionIdColumn}", If(NextDirectionId Is Nothing, CObj(DBNull.Value), NextDirectionId))
+            command.ExecuteNonQuery()
+        End Using
+    End Sub
+    Friend Const Direction1 = 1L
+    Friend Const Direction2 = 2L
+    Friend Const Direction3 = 3L
+    Friend Const Direction4 = 4L
+    Friend Const Direction5 = 5L
+    Friend Const Direction6 = 6L
+    Friend Const Direction7 = 7L
+    Friend Const Direction8 = 8L
+    Friend Sub PopulateDirections(connection As SqliteConnection)
+        PopulateDirectionsRecord(connection, Direction1, "North", "N", 1, Direction4, Direction3, Direction2)
+        PopulateDirectionsRecord(connection, Direction2, "East", "E", 1, Direction1, Direction4, Direction3)
+        PopulateDirectionsRecord(connection, Direction3, "South", "S", 1, Direction2, Direction1, Direction4)
+        PopulateDirectionsRecord(connection, Direction4, "West", "W", 1, Direction3, Direction2, Direction1)
+        PopulateDirectionsRecord(connection, Direction5, "Up", "U", 0, Nothing, Direction6, Nothing)
+        PopulateDirectionsRecord(connection, Direction6, "Down", "D", 0, Nothing, Direction5, Nothing)
+        PopulateDirectionsRecord(connection, Direction7, "In", "In", 0, Nothing, Direction8, Nothing)
+        PopulateDirectionsRecord(connection, Direction8, "Out", "Out", 0, Nothing, Direction7, Nothing)
+    End Sub
+End Module
---
+++ src/KordanorsCabal.Schema/FeatureTypePopulator.vb
@@ -0,0 +1,40 @@
+﻿Module FeatureTypePopulator
+    Private Sub PopulateFeatureTypesRecord(connection As SqliteConnection, FeatureTypeId As Long, FeatureTypeName As String, LocationTypeId As Long, InteractionMode As Long)
+        Using command = New SqliteCommand($"INSERT INTO [{Tables.FeatureTypes}]([{Columns.FeatureTypeIdColumn}], [{Columns.FeatureTypeNameColumn}], [{Columns.LocationTypeIdColumn}], [{Columns.InteractionModeColumn}]) VALUES (@{Columns.FeatureTypeIdColumn}, @{Columns.FeatureTypeNameColumn}, @{Columns.LocationTypeIdColumn}, @{Columns.InteractionModeColumn});", connection)
+            command.Parameters.AddWithValue($"@{Columns.FeatureTypeIdColumn}", FeatureTypeId)
+            command.Parameters.AddWithValue($"@{Columns.FeatureTypeNameColumn}", FeatureTypeName)
+            command.Parameters.AddWithValue($"@{Columns.LocationTypeIdColumn}", LocationTypeId)
+            command.Parameters.AddWithValue($"@{Columns.InteractionModeColumn}", InteractionMode)
+            command.ExecuteNonQuery()
+        End Using
+    End Sub
+    Friend Const FeatureType1 = 1L
+    Friend Const FeatureType2 = 2L
+    Friend Const FeatureType3 = 3L
+    Friend Const FeatureType4 = 4L
+    Friend Const FeatureType5 = 5L
+    Friend Const FeatureType6 = 6L
+    Friend Const FeatureType7 = 7L
+    Friend Const FeatureType8 = 8L
+    Friend Const FeatureType9 = 9L
+    Friend Const InteractionMode4 = 4
+    Friend Const InteractionMode5 = 5
+    Friend Const InteractionMode6 = 6
+    Friend Const InteractionMode7 = 7
+    Friend Const InteractionMode8 = 8
+    Friend Const InteractionMode9 = 9
+    Friend Const InteractionMode10 = 10
+    Friend Const InteractionMode11 = 11
+    Friend Const InteractionMode12 = 12
+    Friend Sub PopulateFeatureTypes(connection As SqliteConnection)
+        PopulateFeatureTypesRecord(connection, FeatureType1, "Zooperdan the Elder", LocationType1, InteractionMode4)
+        PopulateFeatureTypesRecord(connection, FeatureType2, "Graham the Innkeeper", LocationType2, InteractionMode5)
+        PopulateFeatureTypesRecord(connection, FeatureType3, "Yermom the Drunk", LocationType2, InteractionMode6)
+        PopulateFeatureTypesRecord(connection, FeatureType4, "Sander the Chicken", LocationType2, InteractionMode7)
+        PopulateFeatureTypesRecord(connection, FeatureType5, """Honest"" Dan", LocationType2, InteractionMode8)
+        PopulateFeatureTypesRecord(connection, FeatureType6, "Marcus the Black Mage", LocationType2, InteractionMode9)
+        PopulateFeatureTypesRecord(connection, FeatureType7, "Samuli the Blacksmith", LocationType2, InteractionMode10)
+        PopulateFeatureTypesRecord(connection, FeatureType8, "Nihilist Healer Marten", LocationType2, InteractionMode12)
+        PopulateFeatureTypesRecord(connection, FeatureType9, "David the Constable", LocationType2, InteractionMode11)
+    End Sub
+End Module
