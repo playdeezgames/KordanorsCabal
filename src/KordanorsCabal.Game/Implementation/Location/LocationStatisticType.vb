d35e8b8191d99bc161bdb69a71e840c73be4033f
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-10-23 08:23:34 -0500

    LocationStatisticType

--- src/KordanorsCabal.Game/Implementation/Location/LocationStatisticType.vb
+++ src/KordanorsCabal.Game/Implementation/Location/LocationStatisticType.vb
@@ -3,3 +3,13 @@
     DungeonColumn
     DungeonRow
 End Enum
+Public Class LocationStatisticType
+    Inherits BaseThingie
+    Implements ILocationStatisticType
+    Public Sub New(worldData As IWorldData, id As Long)
+        MyBase.New(worldData, id)
+    End Sub
+    Shared Function FromId(worldData As IWorldData, id As Long?) As ILocationStatisticType
+        Return If(id.HasValue, New LocationStatisticType(worldData, id.Value), Nothing)
+    End Function
+End Class
