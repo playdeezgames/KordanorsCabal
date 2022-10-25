b9a0408c83ebbb48b89afced35876baca830d18e
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-10-25 05:05:04 -0500

    moved item kind populator into its own module

--- src/KordanorsCabal.Schema/Populator.vb
+++ src/KordanorsCabal.Schema/Populator.vb
@@ -51,26 +51,4 @@ Public Module Populator
         PopulateLores(connection)
         PopulateItemLores(connection)
     End Sub
-    Public Const ItemKind1 = 1L
-    Private Sub PopulateItemKinds(connection As SqliteConnection)
-        PopulateItemKindsRecord(connection, ItemKind1, "Trophy")
-    End Sub
-
-    Private Sub PopulateItemKindsRecord(connection As SqliteConnection, itemKindId As Long, itemKindName As String)
-        Using command = New SqliteCommand(
-                $"INSERT INTO [{Tables.ItemKinds}]
-                (
-                    [{Columns.ItemKindIdColumn}], 
-                    [{Columns.ItemKindNameColumn}]
-                ) 
-                VALUES 
-                (
-                    @{Columns.ItemKindIdColumn}, 
-                    @{Columns.ItemKindNameColumn}
-                );", connection)
-            command.Parameters.AddWithValue($"@{Columns.ItemKindIdColumn}", itemKindId)
-            command.Parameters.AddWithValue($"@{Columns.ItemKindNameColumn}", itemKindName)
-            command.ExecuteNonQuery()
-        End Using
-    End Sub
 End Module
